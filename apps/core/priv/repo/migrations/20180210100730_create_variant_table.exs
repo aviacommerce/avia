a35431b3afd06fbd59407ea6f121fcb1b21487ea
Ashish Singh <aitashish173@gmail.com>
2018-02-12 04:50:37 +0530

    [WIP] Updated variants table
    * Relationships with other tables pending
    * Add validations pending

---
+++ apps/core/priv/repo/migrations/20180210100730_create_variant_table.exs
@@ -0,0 +1,22 @@
+defmodule Core.Repo.Migrations.CreateVariantTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:snitch_variants) do
+      add :sku, :string, null: false, default: ""
+      add :weight, :decimal, precision: 8, scale: 2, default: 0.0
+      add :height, :decimal, precision: 8, scale: 2
+      add :width, :decimal, precision: 8, scale: 2
+      add :depth, :decimal, precision: 8, scale: 2
+      add :is_master, :boolean, default: false
+      add :cost_price, :decimal, precision: 10, scale: 2
+      add :position, :integer
+      add :cost_currency, :string
+      add :track_inventory, :boolean, default: true
+      add :discontinue_on, :naive_datetime
+
+      # Not adding any association ids
+      timestamps()
+    end
+  end
+end
