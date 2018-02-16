8a23f6365128454518297020a44e1d988b6ef54c
Ananya Bahadur <ananya95@gmail.com>
2018-02-16 16:02:33 +0530

    Use ex_money's Ecto type for price. (#3)
    * Use ex_money's Ecto type for price.
    
    * removed `:cost_currency`
    * modified `:cost_price`
      - So is this cost price or selling price??
      - And why is this column needed if there is a snitch_prices table?
    
    * Corrected schema, added unique index on `:sku`

---
+++ apps/core/priv/repo/migrations/20180213103123_add_money_with_currency_type_to_postgres.exs
@@ -0,0 +1,13 @@
+defmodule Core.Repo.Migrations.AddMoneyWithCurrencyTypeToPostgres do
+  use Ecto.Migration
+
+  def up do
+    execute """
+      CREATE TYPE public.money_with_currency AS (currency_code char(3), amount numeric(20,8))
+    """
+  end
+
+  def down do
+    execute "DROP TYPE public.money_with_currency"
+  end
+end
