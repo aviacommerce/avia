54154abf237cd53a7e4740bd64e27eacc0c318ec
Ananya Bahadur <ananya95@gmail.com>
2018-02-16 15:38:08 +0530

    Adds order changesets to create and update totals
    * Also adds tests cases to cover valid and invalid line_items
    * `ensure_unique_line_items` ensures that each line_item has a different
    variant from every other line_item

--- apps/core/lib/core/snitch/order.ex
+++ apps/core/lib/core/snitch/order.ex
@@ -5,6 +5,8 @@ defmodule Core.Snitch.Order do
 
   use Ecto.Schema
 
+  alias Core.Snitch.{LineItem}
+
   import Ecto.Changeset
 
   @type t :: %__MODULE__{}
@@ -35,21 +37,53 @@ defmodule Core.Snitch.Order do
     timestamps()
   end
 
-  @required_fields ~w(slug state total user_id billing_address_id shipping_address_id)a
+  @required_fields ~w(slug state user_id billing_address_id shipping_address_id)a
   @optional_fields ~w()a
 
   @spec changeset(__MODULE__.t(), map()) :: Ecto.Changeset.t()
-  def changeset(order, params) do
+  defp changeset(order, params) do
     order
     |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
     |> foreign_key_constraint(:user_id)
-    |> foreign_key_constraint(:billing_shipping_id)
-    |> foreign_key_constraint(:shipping_shipping_id)
+    |> foreign_key_constraint(:billing_address_id)
+    |> foreign_key_constraint(:shipping_address_id)
   end
 
-  def create_changeset(order, params, _line_item_changesets) do
+  def create_changeset(order, params) do
     order
     |> changeset(params)
+    |> validate_required([:line_items])
+    |> cast_assoc(:line_items, with: &LineItem.create_changeset/2)
+    |> ensure_unique_line_items()
   end
+
+  def update_product_totals_changeset(order_with_line_items) do
+    order_with_line_items
+    |> validate_required([:line_items])
+    |> LineItem.update_totals()
+  end
+
+  def ensure_unique_line_items(%Ecto.Changeset{valid?: true} = order_changeset) do
+    {:ok, line_items} = fetch_change(order_changeset, :line_items)
+
+    items_are_unique? =
+      Enum.reduce_while(line_items, MapSet.new(), fn item, map_set ->
+        v_id = item.changes.variant_id
+
+        if MapSet.member?(map_set, v_id) do
+          {:halt, false}
+        else
+          {:cont, MapSet.put(map_set, v_id)}
+        end
+      end)
+
+    if items_are_unique? do
+      order_changeset
+    else
+      add_error(order_changeset, :duplicate_variants, "line_items must have unique variant_ids")
+    end
+  end
+
+  def ensure_unique_line_items(order_changeset), do: order_changeset
 end
