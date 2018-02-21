435e4d802bde6df1bab2d7ba1d0cdc56d10a3abc
Arjun Singh <arjun289@users.noreply.github.com>
2018-02-21 16:12:07 +0530

    create country and state enitites, migration and seed (#4)
    Create State, Country relations and seed data for it

---
+++ apps/core/priv/repo/migrations/20180212120541_create_snitch_countries.exs
@@ -0,0 +1,16 @@
+defmodule Core.Repo.Migrations.CreateSnitchCountries do
+  use Ecto.Migration
+
+  def change do
+    create table(:snitch_countries) do
+      add :iso_name, :string
+      add :iso, :string
+      add :iso3, :string
+      add :name, :string
+      add :numcode, :string
+      add :states_required, :boolean, default: false
+      
+      timestamps()
+    end
+  end
+end
