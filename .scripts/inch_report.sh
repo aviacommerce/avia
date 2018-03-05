ea592c65f0ae4c396e5d496ff1458df03ca5922d
Ananya Bahadur <ananya95+git@gmail.com>
2018-03-05 12:30:51 +0530

    Inch Reports are made only for master and develop
    * This may not work! Because these branches recieve commits only via
    PRs.

--- .scripts/inch_report.sh
+++ .scripts/inch_report.sh
@@ -2,9 +2,15 @@
 
 set -ev
 
+allowed_branches="^(master)|(develop)$"
+
 if [ $TRAVIS_PULL_REQUEST = false ]; then
-    env MIX_ENV=docs mix deps.get
-    env MIX_ENV=docs mix inch.report
+    if [[ $TRAVIS_BRANCH =~ $allowed_branches ]]; then
+        env MIX_ENV=docs mix deps.get
+        env MIX_ENV=docs mix inch.report
+    else
+        echo "Skipping Inch CI report because this branch does not match on /$allowed_branches/"
+    fi
 else
-    echo "Skipping Inch report because this is a PR build"
+    echo "Skipping Inch CI report because this is a PR build"
 fi
