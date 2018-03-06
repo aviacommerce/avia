79b3744c9f2634b78eb809bf6fefabdb90bed76c
Arjun Singh <arjun289@users.noreply.github.com>
2018-03-06 10:29:08 +0530

    [Restructure] Snitch as per discussion (#18)
    * moved schemas to schema folder
    
    * moved data and domain to core
    
    * modify schema, model and tests
    
    * modify state schema, query helper, seed
    
    Modified state schema to remove redundant foreign key
    country_id. Modified query helper module name. Use one
    liner in seed to call seed data.
    
    * modify snitch to remove core from module name

---
+++ apps/snitch_core/lib/core/data/model/stock/stock.ex
@@ -0,0 +1,14 @@
+defmodule Snitch.Core.Data.Model.Stock do
+  @moduledoc """
+    Interface for handling DB related changes
+  """
+
+  defmacro __using__(_) do
+    quote do
+      alias Snitch.Data.Model.{
+        StockItem,
+        StockLocation
+      }
+    end
+  end
+end
