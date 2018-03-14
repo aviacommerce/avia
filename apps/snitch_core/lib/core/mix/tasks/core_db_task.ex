Euklidian-Space/snitch

add custom mix tasks for reset and seed

apps/core/lib/mix/tasks/core_db_task.ex
@@ -1,0 +1,46 @@
+defmodule Mix.Tasks.Core.Resetdb do
+  @moduledoc """
+  Custom mix task to reset db.
+  ```
+    mix core.resetdb
+  ```
+  """
+  use Mix.Task
+
+  def run(_) do
+    System.cmd(
+      "mix",
+      ["ecto.drop"],
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    )
+
+    System.cmd(
+      "mix",
+      ["ecto.create"],
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    )
+
+    System.cmd(
+      "mix",
+      ["ecto.migrate"],
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    )
+  end
+end
+
+defmodule Mix.Tasks.Core.Seed do
+  @moduledoc """
+    Custom mix task to seed db.
+    ```
+      mix core.seed
+    ```
+  """
+  use Mix.Task
+
+  def run(_) do
+    Mix.Task.run("run", ["./apps/core/priv/repo/seeds.exs"])
+  end
+end

aviacommerce/avia

apps/snitch_core/lib/core/mix/tasks/core_db_task.ex
@@ -1,0 +1,46 @@
+defmodule Mix.Tasks.Snitch.Resetdb do
+  @moduledoc """
+  Custom mix task to reset db.
+  ```
+    mix snitch.resetdb
+  ```
+  """
+  use Mix.Task
+
+  def run(_) do
+    System.cmd(
+      "mix",
+      ["ecto.drop"],
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    )
+
+    System.cmd(
+      "mix",
+      ["ecto.create"],
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    )
+
+    System.cmd(
+      "mix",
+      ["ecto.migrate"],
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line)
+    )
+  end
+end
+
+defmodule Mix.Tasks.Snitch.Seed do
+  @moduledoc """
+    Custom mix task to seed db.
+    ```
+      mix snitch.seed
+    ```
+  """
+  use Mix.Task
+
+  def run(_) do
+    Mix.Task.run("run", ["./apps/snitch_core/priv/repo/seeds.exs"])
+  end
+end
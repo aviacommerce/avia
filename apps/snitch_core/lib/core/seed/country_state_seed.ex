1ed9c03d4ba71c5e45240b7ad74142bcb7ca35c4
arjun289 <arjun289singh@gmail.com>
2018-02-22 01:07:59 +0530

    add custom mix tasks for reset and seed

--- apps/snitch_core/lib/core/seed/country_state_seed.ex
+++ apps/snitch_core/lib/core/seed/country_state_seed.ex
@@ -4,30 +4,51 @@ defmodule Snitch.Seed.CountryState do
   entitites.
   """
 
+  import Ecto.Query
   alias Worldly.Country, as: WorldCountry
   alias Worldly.Region, as: WorldRegion
-  alias Snitch.Data.Schema.Country
   alias Snitch.Repo
+  alias Snitch.Data.Schema.Country
 
   def seed_countries_and_states! do
-    Enum.each(WorldCountry.all(), fn country -> seed_country_data(country) end)
+    insert_countries()
   end
 
-  def seed_country_data(country) do
-    change = Country.changeset(%Country{}, to_param(country))
-    inserted_country = Repo.insert!(change)
+  def insert_countries do
+    uniq_countries = unique_countries()
 
-    if country.has_regions do
-      Enum.each(WorldRegion.regions_for(country), fn region ->
-        seed_state_data(inserted_country, region)
+    countries =
+      WorldCountry.all()
+      |> Enum.reject(&Map.get(uniq_countries, &1.name))
+
+    insertable_countries =
+      countries
+      |> Enum.map(fn country -> to_param(country) end)
+
+    {_, countries_ids} =
+      Repo.insert_all(
+        "snitch_countries",
+        insertable_countries,
+        on_conflict: :nothing,
+        returning: [:id]
+      )
+
+    country_ids = Stream.map(countries_ids, fn x -> x.id end)
+
+    countries
+    |> Stream.map(&WorldRegion.regions_for/1)
+    |> Stream.zip(country_ids)
+    |> Enum.map(fn {statelist, id} ->
+      Enum.map(statelist, fn state ->
+        to_param(state, id)
       end)
-    end
+    end)
+    |> List.flatten()
+    |> insert_and_map_states
   end
 
-  def seed_state_data(country, state) do
-    country
-    |> Ecto.build_assoc(:states, to_param(state))
-    |> Repo.insert!()
+  def insert_and_map_states(states) do
+    Repo.insert_all("snitch_states", states, on_conflict: :nothing)
   end
 
   defp to_param(%WorldCountry{
@@ -43,11 +64,29 @@ defmodule Snitch.Seed.CountryState do
       iso3: iso3,
       numcode: numcode,
       states_required: has_regions,
-      iso_name: String.upcase(name)
+      iso_name: String.upcase(name),
+      inserted_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
     }
   end
 
-  defp to_param(%WorldRegion{code: abbr, name: name}) do
-    %{abbr: to_string(abbr), name: to_string(name)}
+  defp to_param(%WorldRegion{code: abbr, name: name}, id) do
+    %{
+      abbr: to_string(abbr),
+      name: to_string(name),
+      country_id: id,
+      inserted_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()
+    }
+  end
+
+  defp unique_countries() do
+    stream =
+      Country
+      |> select([c], {c.name, true})
+      |> Repo.stream()
+
+    {:ok, countries} = Repo.transaction(fn -> Enum.into(stream, %{}) end)
+    countries
   end
 end
