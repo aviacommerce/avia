44dcf599ca5c72cc3624853a395344593269a24f
data-kid <gabriel.grover@gmail.com>
2018-03-14 12:17:25 -0700

    changed update tests to reflect the update use case more closely

--- apps/snitch_core/test/data/schema/user_test.exs
+++ apps/snitch_core/test/data/schema/user_test.exs
@@ -3,7 +3,6 @@ defmodule Snitch.Data.Schema.UserTest do
   use Snitch.DataCase
 
   alias Snitch.Data.Schema.User
-  alias Snitch.Data.Schema.Address
 
   @valid_attrs %{
     first_name: "John",
@@ -85,35 +84,37 @@ defmodule Snitch.Data.Schema.UserTest do
 
   describe "Update User" do
     setup  do
-      cs = User.changeset(%User{}, @valid_attrs, :create)
-      [cs: cs]
+      user = User.changeset(%User{}, @valid_attrs, :create)
+        |> Ecto.Changeset.apply_changes()
+
+      [user: user]
     end
 
-    test "requires valid email", %{cs: changeset} do
+    test "requires valid email", %{user: user} do
       params = %{email: "john_example.com"}
-      %{valid?: validity, errors: errs} = User.changeset(changeset, params, :update)
+      %{valid?: validity, errors: errs} = User.changeset(user, params, :update)
       refute validity
       assert Keyword.get(errs, :email) == {"has invalid format", [validation: :format]}
     end
 
-    test "password is stored as hash", %{cs: changeset} do
+    test "password is stored as hash", %{user: user} do
       params = %{password: "secret1234", password_confirmation: "secret1234"}
-      %{valid?: validity, changes: changes} = User.changeset(changeset, params, :update)
+      %{valid?: validity, changes: changes} = User.changeset(user, params, :update)
       assert validity
       assert Map.has_key?(changes, :password_hash)
       refute Map.has_key?(changes, :password)
     end
 
-    test "password must have 8 characters", %{cs: changeset} do
+    test "password must have 8 characters", %{user: user} do
       params = %{password: "secret", password_confirmation: "secret"}
-      %{valid?: validity, errors: [err]} = User.changeset(changeset, params, :update)
+      %{valid?: validity, errors: [err]} = User.changeset(user, params, :update)
       refute validity
       assert err == {:password, {"The password must be 8 characters long.", validation: :password}}
     end
 
-    test "password confirmation required", %{cs: changeset} do
+    test "password confirmation required", %{user: user} do
       params = %{password: "password1234", password_confirmation: "does not match"}
-      %{valid?: validity, errors: [err]} = User.changeset(changeset, params, :update)
+      %{valid?: validity, errors: [err]} = User.changeset(user, params, :update)
       refute validity
       assert err == {:password_confirmation, {"does not match confirmation", [validation: :confirmation]}}
     end
