b28e5aa9c2eb30fbdc1d77588eec5b27bb7730ac
Ananya Bahadur <ananya95+git@gmail.com>
2018-03-15 10:43:49 +0530

    Add `ShippingMethod` schema and tests
    * A shipping_method may have zero or more zones.
    * Zones can be associated ONLY by providing a list of zone structs. A
    list of zone_ids will not suffice.
      - That's how put_assoc works in a man-to-many relation.
    * shipping_method cannot have duplicate zones.
    * shipping_method can have zones of different types

---
+++ apps/snitch_core/test/data/schema/shipping_method_test.exs
@@ -0,0 +1,88 @@
+defmodule Snitch.Data.Schema.ShippingMethodTest do
+  use ExUnit.Case, async: true
+  use Snitch.DataCase
+
+  import Snitch.Factory
+
+  alias Snitch.Data.Schema.ShippingMethod
+
+  @valid_params %{
+    slug: "shipping-method-0",
+    name: "FREE One Day delivery",
+    description: "*Conditions apply :)"
+  }
+
+  @tag state_zone_count: 3
+  describe "create shipping_method" do
+    setup :zones
+
+    test "changeset with valid params and zones", %{zones: zones} do
+      %{valid?: validity} =
+        changeset = ShippingMethod.changeset(%ShippingMethod{}, @valid_params, zones, :create)
+
+      assert validity
+      assert Enum.all?(changeset.changes.zones, fn cset -> cset.action == :update end)
+    end
+
+    test "changeset with valid params and no zones" do
+      %{valid?: validity} =
+        ShippingMethod.changeset(%ShippingMethod{}, @valid_params, [], :create)
+
+      assert validity
+    end
+
+    test "name, slug, description cannot be blank", %{zones: zones} do
+      %{valid?: validity} =
+        changeset = ShippingMethod.changeset(%ShippingMethod{}, %{}, zones, :create)
+
+      refute validity
+
+      assert %{
+               description: ["can't be blank"],
+               name: ["can't be blank"],
+               slug: ["can't be blank"]
+             } = errors_on(changeset)
+    end
+
+    @tag state_zone_count: 1, country_zone_count: 1
+    test "allow zones of different types", %{zones: zones} do
+      %{valid?: validity} =
+        changeset = ShippingMethod.changeset(%ShippingMethod{}, @valid_params, zones, :create)
+
+      assert validity
+    end
+  end
+
+  @tag country_zone_count: 3
+  describe "update shipping_method" do
+    setup :zones
+    setup :shipping_method
+
+    test "slugs must be unique" do
+      cs = ShippingMethod.changeset(%ShippingMethod{}, @valid_params, [], :create)
+      assert {:error, changeset} = Repo.insert(cs)
+      assert %{slug: ["has already been taken"]} = errors_on(changeset)
+    end
+
+    test "allow zones to be replaced", %{shipping_method: sm} do
+      [zones: new_zones] = zones(%{country_zone_count: 2})
+
+      cs = %{valid?: validity} = ShippingMethod.changeset(sm, %{}, new_zones, :update)
+      assert validity
+      assert Enum.all?(cs.changes.zones, fn cset -> cset.action == :update end)
+      assert {:ok, _} = Repo.update(cs)
+    end
+
+    test "allow zones to be removed", %{shipping_method: sm} do
+      cs = %{valid?: validity} = ShippingMethod.changeset(sm, %{}, [], :update)
+      assert validity
+      assert {:ok, _} = Repo.update(cs)
+    end
+  end
+
+  defp shipping_method(%{zones: zones}) do
+    cs = ShippingMethod.changeset(%ShippingMethod{}, @valid_params, zones, :create)
+    {:ok, sm} = Repo.insert(cs)
+    [shipping_method: sm]
+  end
+end
