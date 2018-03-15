cf00f4303227b056e143a7af3f8c6345dee2773b
Ananya Bahadur <ananya95+git@gmail.com>
2018-03-15 10:43:49 +0530

    Add `ShippingMethod` schema and tests
    * A shipping_method may have zero or more zones.
    * Zones can be associated ONLY by providing a list of zone structs. A
    list of zone_ids will not suffice.
      - That's how put_assoc works in a man-to-many relation.
    * shipping_method cannot have duplicate zones.
    * shipping_method can have zones of different types

--- apps/snitch_core/test/support/factory/zone.ex
+++ apps/snitch_core/test/support/factory/zone.ex
@@ -7,9 +7,20 @@ defmodule Snitch.Factory.Zone do
 
       def zone_factory do
         %Zone{
-          name: sequence("area", fn area_code -> "-#{area_code + 50}" end),
+          name: sequence(:area, fn area_code -> "area-#{area_code + 50}" end),
           description: "Does area-51 exist?"
         }
+      end
+
+      def zones(context) do
+        sz_count = Map.get(context, :state_zone_count, 0)
+        cz_count = Map.get(context, :country_zone_count, 0)
+
+        [
+          zones:
+            insert_list(sz_count, :zone, zone_type: "S") ++
+              insert_list(cz_count, :zone, zone_type: "C")
+        ]
       end
     end
   end
