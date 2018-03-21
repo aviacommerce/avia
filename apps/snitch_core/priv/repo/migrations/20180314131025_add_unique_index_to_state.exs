ec38a65d48c0bb2da08a128af847e9c9d870f7b2
Gopal Shimpi <gopalshimpi@gmail.com>
2018-03-21 18:11:42 +0530

    minor changes in migration

--- apps/snitch_core/priv/repo/migrations/20180314131025_add_unique_index_to_state.exs
+++ apps/snitch_core/priv/repo/migrations/20180314131025_add_unique_index_to_state.exs
@@ -2,7 +2,6 @@ defmodule Snitch.Repo.Migrations.AddUniqueIndexToState do
   use Ecto.Migration
 
   def change do
-    create unique_index(:snitch_states, [:abbr, :country_id], 
-    message: "(:country_id, :abbr)has already been taken")
+    create unique_index(:snitch_states, [:abbr, :country_id])
   end
 end
