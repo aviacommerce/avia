a5e3f21c9c6722fd125a50da64c331f4c485cee9
Gopal Shimpi <gopalshimpi@gmail.com>
2018-03-22 19:54:39 +0530

    Added test for states
    1. Added test for unique constraints (country_id and abbr)
    2. Modified migration.

--- apps/snitch_core/priv/repo/migrations/20180314131025_add_unique_index_to_state.exs
+++ apps/snitch_core/priv/repo/migrations/20180314131025_add_unique_index_to_state.exs
@@ -2,6 +2,8 @@ defmodule Snitch.Repo.Migrations.AddUniqueIndexToState do
   use Ecto.Migration
 
   def change do
-    create unique_index(:snitch_states, [:abbr, :country_id])
+    create(
+      unique_index(:snitch_states, [:abbr, :country_id], name: :snitch_state_abbr_and_country_id)
+    )
   end
 end
