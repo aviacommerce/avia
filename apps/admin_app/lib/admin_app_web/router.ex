5f62a52d1e84b5ca6b2dfdc4cdd2b32a00d3d88f
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-04-23 15:10:55 +0530

    admin app init

---
+++ apps/admin_app/lib/admin_app_web/router.ex
@@ -0,0 +1,27 @@
+defmodule AdminAppWeb.Router do
+  use AdminAppWeb, :router
+
+  pipeline :browser do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+  end
+
+  pipeline :api do
+    plug(:accepts, ["json"])
+  end
+
+  scope "/", AdminAppWeb do
+    # Use the default browser stack
+    pipe_through(:browser)
+
+    get("/", PageController, :index)
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AdminAppWeb do
+  #   pipe_through :api
+  # end
+end
