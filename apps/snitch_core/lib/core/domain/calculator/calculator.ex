a3d245b6034439ad2275814a3a6cc2241b8df839
arjun289 <arjun289singh@gmail.com>
2018-05-09 22:48:28 +0530

    Add TaxRate, UserConfig helper, Calculator (#72)
    `TaxRate` schema and model
    --------------------------
    
    `TaxRate`s associate zones with calculators and provide additional
    arguments to it using the `:value` field.
    As of now `:value` only accepts a `Decimal` type.
    `TaxRate`s are soft deleted. For a hard delete use `Ecto.Repo.delete`.
    [delivers #157502102]
    
    `UserConfig` helper
    -------------------
    
    This interfaces only with `Application` config, but can interface with
    others later, acting as an abstraction for fetching user config.
    Also helps run the tests async (using `mox`) :)
    [delivers #157502146]
    
    `Calculator`, base and default tax calculator
    ---------------------------------------------
    
    `Calculator` behavior defines a single callback:
    * `compute(value :: map, item :: map | nil) :: Money.t`
      `value` refers to rate/rates to be used for
       calculation, `item` is something on which
       we wish to compute some amount.
    
    [delivers #157147786]

---
+++ apps/snitch_core/lib/core/domain/calculator/calculator.ex
@@ -0,0 +1,32 @@
+defmodule Snitch.Domain.Calculator do
+  @moduledoc """
+  Base Calculator module.
+  """
+
+  @defaults Application.get_env(:snitch_core, :defaults_module)
+  @user_config Application.get_env(:snitch_core, :user_config_module)
+
+  @doc """
+  Returns a Money struct after performing computation.
+
+  Takes as input `item` and `value`.
+  > Note
+  > `value` refers to rate/rates to be used for computation.
+  > `item` is something on which the computation is done.
+  """
+  @callback compute(value :: map, item :: map | nil) :: Money.t()
+
+  @doc """
+  Returns a list of calculators.
+
+  The list includes the calculators defined in the `core` app as well as, 
+  those defined by the `user` under `snitch_core` config.
+  """
+  @spec list :: [atom]
+  def list do
+    case @defaults.fetch(:calculators) do
+      {:ok, value} -> value ++ @user_config.get(:calculators)
+      {:error, _} = value -> value
+    end
+  end
+end
