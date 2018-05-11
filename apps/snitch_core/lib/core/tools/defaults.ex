4aa72a2da0cda8b95618045694014d8fa432b624
arjun289 <arjun289singh@gmail.com>
2018-05-11 11:17:53 +0530

    modify defaults helper
    * modify fetch/1 to return only defaults
     - add tests
    * remove config_app
    * [delivers #157502173]

--- apps/snitch_core/lib/core/tools/defaults.ex
+++ apps/snitch_core/lib/core/tools/defaults.ex
@@ -3,22 +3,18 @@ defmodule Snitch.Tools.Defaults do
   Helper to fetch configured defaults for `Snitch`.
   """
 
-  @callback config_app :: atom
   @callback fetch(atom) :: term
   @callback validate_config :: {:ok | :error, term}
 
-  def config_app, do: Application.get_env(:snitch_core, :config_app)
-
   def fetch(key) do
-    with {:ok, core_config_app} <- Application.fetch_env(:snitch_core, :config_app),
-         {:ok, defaults} <- Application.fetch_env(core_config_app, :defaults) do
+    with {:ok, defaults} <- Application.fetch_env(:snitch_core, :defaults) do
       case Keyword.fetch(defaults, key) do
         {:ok, _} = value -> value
         :error -> {:error, "default '#{key}' not set"}
       end
     else
       :error ->
-        {:error, "Could not fetch any 'defaults' from config under ':core_config_app'"}
+        {:error, "Could not fetch any 'defaults'"}
     end
   end
 
