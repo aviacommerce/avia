fd11fbeef86293991977fd585ee50396fe645823
Ananya Bahadur <ananya95+git@gmail.com>
2018-05-26 15:23:24 +0530

    Seed more addresses, add/remove constraints
    Migrations
    ----------
    * Order can be deleted leaving the addresses intact
    * StockLocation admin_names must be unique
    
    Update postgres version (9.4 -> 9.5)
    
    [delivers #157925979]

--- apps/snitch_core/priv/repo/seed/users.ex
+++ apps/snitch_core/priv/repo/seed/users.ex
@@ -12,15 +12,6 @@ defmodule Snitch.Seed.Users do
   @user_passwd Argon2.hashpwsalt("avenger")
   @admin_passwd Argon2.hashpwsalt("wizard")
 
-  @address %Address{
-    first_name: "Tony",
-    last_name: "Stark",
-    address_line_1: "10-8-80 Malibu Point",
-    zip_code: "90265",
-    city: "Malibu",
-    phone: "1234567890"
-  }
-
   def seed_users! do
     users = [
       user("Harry", "Potter", "admin@snitch.com", @admin_passwd, true),
@@ -45,8 +36,44 @@ defmodule Snitch.Seed.Users do
   end
 
   def seed_address! do
-    california = State.get(%{code: "US-CA"})
-    us = Country.get(%{iso: "US"})
-    Repo.insert!(%{@address | state_id: california.id, country_id: us.id})
+    Repo.delete_all(Address)
+
+    Enum.map(
+      [
+        %Address{
+          first_name: "Tony",
+          last_name: "Stark",
+          address_line_1: "10-8-80 Malibu Point",
+          zip_code: "90265",
+          city: "Malibu",
+          phone: "1234567890",
+          state_id: State.get(%{code: "US-CA"}).id,
+          country_id: Country.get(%{iso: "US"}).id
+        },
+        %Address{
+          first_name: "noname",
+          last_name: "noname",
+          address_line_1: "somewhere",
+          address_line_2: "street",
+          zip_code: "00000",
+          city: "Pune",
+          phone: "1234567890",
+          state_id: State.get(%{code: "IN-MH"}).id,
+          country_id: Country.get(%{iso: "IN"}).id
+        },
+        %Address{
+          first_name: "noname",
+          last_name: "noname",
+          address_line_1: "nowhere",
+          address_line_2: "street",
+          zip_code: "11111",
+          city: "Patna",
+          phone: "1234567890",
+          state_id: State.get(%{code: "IN-BR"}).id,
+          country_id: Country.get(%{iso: "IN"}).id
+        }
+      ],
+      &Repo.insert!/1
+    )
   end
 end
