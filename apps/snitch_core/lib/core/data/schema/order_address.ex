1ac3c183282a1ab699c40d1a9c9fc6eecd56309f
blackode <codeme@gmx.com>
2018-06-26 02:26:15 +0530

    updated credo related issues

--- apps/snitch_core/lib/core/data/schema/order_address.ex
+++ apps/snitch_core/lib/core/data/schema/order_address.ex
@@ -1,4 +1,6 @@
 defmodule Snitch.Data.Schema.OrderAddress do
+  @moduledoc false
+
   use Snitch.Data.Schema
 
   alias Snitch.Data.Schema.{Country, State}
@@ -6,8 +8,6 @@ defmodule Snitch.Data.Schema.OrderAddress do
 
   @required_fields ~w(first_name last_name address_line_1 city zip_code country_id)a
   @cast_fields ~w(phone alternate_phone state_id)a ++ @required_fields
-
-  @moduledoc false
 
   embedded_schema do
     field(:first_name, :string)
