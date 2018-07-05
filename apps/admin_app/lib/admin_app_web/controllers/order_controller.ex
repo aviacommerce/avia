08c5f27b5669965bcb2459d65a6952440d22f963
Anwesh <kanishkablack@gmx.com>
2018-07-05 19:14:56 +0530

    udpate line items

--- apps/admin_app/lib/admin_app_web/controllers/order_controller.ex
+++ apps/admin_app/lib/admin_app_web/controllers/order_controller.ex
@@ -32,13 +32,39 @@ defmodule AdminAppWeb.OrderController do
   end
 
   def edit(conn, params) do
-    order = load_order(%{number: params["number"]})
+    order = load_order(%{number: params["order_number"]})
 
-    render(conn, "show.html", %{order: order, search: []})
+    update_item = params["update"]
+    line_items = remove_line_item(update_item, order.line_items)
+    update_item = List.first(fetch_line_item(update_item, order.line_items))
+
+    render(conn, "edit.html", %{order: order, search: [], update_item: update_item})
+  end
+
+  def update_line_item(conn, params) do
+    order = load_order(%{number: params["order_number"]})
+
+    update_item = params["update"]
+    line_items = remove_line_item(update_item, order.line_items)
+    update_item = List.first(fetch_line_item(update_item, order.line_items))
+    updated_list = [%{quantity: params["quantity"], id: update_item.id} | line_items || []]
+
+    case Order.update(%{line_items: updated_list}, order) do
+      {:ok, order} ->
+        redirect(conn, to: "/orders/#{order.number}")
+
+      {:error, error} ->
+        %{errors: [line_items: {error_text, _}]} = error
+        conn = put_flash(conn, :error, error_text)
+        redirect(conn, to: "/orders/#{order.number}")
+    end
+
+    render(conn, "edit.html", %{order: order, search: [], update_item: update_item})
   end
 
   def add(conn, params) do
     order = load_order(%{number: params["order_number"]})
+
     variant_id = String.to_integer(params["add"])
     quantity = String.to_integer(params["quantity"])
     add_line_item = %{quantity: quantity, variant_id: variant_id}
@@ -55,11 +81,10 @@ defmodule AdminAppWeb.OrderController do
     end
   end
 
-  def update(conn, params) do
+  def remove_item(conn, params) do
     order = load_order(%{number: params["order_number"]})
 
     edit_item = params["edit"]
-
     line_items = remove_line_item(edit_item, order.line_items)
 
     {:ok, order} =
@@ -81,6 +106,7 @@ defmodule AdminAppWeb.OrderController do
 
   def add_address(conn, params) do
     order = load_order(%{number: params["order_number"]})
+
     changeset = Address.changeset(%Address{}, params["address"])
 
     case changeset.valid? do
@@ -110,6 +136,11 @@ defmodule AdminAppWeb.OrderController do
     line_items
     |> Enum.reject(fn %{id: id} -> id == String.to_integer(edit_item) end)
     |> Enum.map(fn item -> Map.from_struct(item) |> Map.drop([:__meta]) end)
+  end
+
+  defp fetch_line_item(edit_item, line_items) do
+    line_items
+    |> Enum.reject(fn %{id: id} -> id != String.to_integer(edit_item) end)
   end
 
   defp struct_to_map(items) do
--- apps/admin_app/lib/admin_app_web/views/order_view.ex
+++ apps/admin_app/lib/admin_app_web/views/order_view.ex
@@ -40,6 +40,7 @@ defmodule AdminAppWeb.OrderView do
       content_tag(:td, line_item.unit_price),
       render_quantity_with_stock(line_item),
       content_tag(:td, line_item.total),
+      render_update_buttons(line_item.id, order),
       render_buttons(line_item.id, order)
     ]
 
@@ -50,16 +51,15 @@ defmodule AdminAppWeb.OrderView do
     [content_tag(:th, content_tag(:i, "", class: "far fa-image")), content_tag(:td, variant.sku)]
   end
 
-  defp render_edit_button() do
-    content_tag(
-      :button,
-      [
-        "remove"
-        # content_tag(:span, tag(:i, class: "far fa-edit"), class: "badge badge-light")
-      ],
-      class: "btn btn-primary",
-      type: "submit"
-    )
+  defp render_update_buttons(item, order) do
+    if is_editable?(order.state) do
+      content_tag(
+        :td,
+        form_tag("/orders/#{order.number}/cart/edit?update=#{item}", method: "post") do
+          content_tag(:button, ["update"], class: "btn btn-primary", type: "submit")
+        end
+      )
+    end
   end
 
   defp render_buttons(item, order) do
@@ -67,7 +67,7 @@ defmodule AdminAppWeb.OrderView do
       content_tag(
         :td,
         form_tag("/orders/#{order.number}/cart?edit=#{item}", method: "post") do
-          render_edit_button()
+          content_tag(:button, ["remove"], class: "btn btn-primary", type: "submit")
         end
       )
     end
@@ -131,5 +131,22 @@ defmodule AdminAppWeb.OrderView do
       end
 
     content_tag(:tr, list)
+  end
+
+  def render_update_item(item, order) do
+    content = [
+      content_tag(:td, item.variant.sku),
+      content_tag(:td, item.variant.selling_price),
+      content_tag(:td, tag(:input, name: "quantity", value: item.quantity)),
+      content_tag(:td, tag(:hidden, name: "variant_id", value: item.variant_id)),
+      content_tag(:td, content_tag(:button, ["Add"], type: "submit"))
+    ]
+
+    list =
+      form_tag("/orders/#{order.number}/cart/update?update=#{item.id}", method: "put") do
+        List.flatten(content)
+      end
+
+    content_tag(:p, list)
   end
 end
