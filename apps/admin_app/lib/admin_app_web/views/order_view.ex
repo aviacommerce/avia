5a0b53310f6a91ee7998bc42f3c187a552e92d67
Anwesh <kanishkablack@gmx.com>
2018-07-05 15:02:30 +0530

    Admin Panel create order, attach address

--- apps/admin_app/lib/admin_app_web/views/order_view.ex
+++ apps/admin_app/lib/admin_app_web/views/order_view.ex
@@ -1,5 +1,8 @@
 defmodule AdminAppWeb.OrderView do
   use AdminAppWeb, :view
+  alias Phoenix.HTML.FormData
+
+  alias Snitch.Data.Model.Order, as: OrderModel
 
   @bootstrap_contextual_class %{
     "cart" => "light",
@@ -32,13 +35,13 @@ defmodule AdminAppWeb.OrderView do
     content_tag(:span, state, class: "badge badge-pill badge-#{color_class}")
   end
 
-  defp render_line_item(line_item, state) do
+  defp render_line_item(line_item, order) do
     content = [
       render_variant(line_item.variant),
       content_tag(:td, line_item.unit_price),
       render_quantity_with_stock(line_item),
-      content_tag(:td, Money.mult!(line_item.unit_price, line_item.quantity)),
-      render_buttons(state)
+      render_update_buttons(line_item.id, order),
+      render_buttons(line_item.id, order)
     ]
 
     content_tag(:tr, List.flatten(content))
@@ -48,19 +51,24 @@ defmodule AdminAppWeb.OrderView do
     [content_tag(:th, content_tag(:i, "", class: "far fa-image")), content_tag(:td, variant.sku)]
   end
 
-  defp render_buttons(state) do
-    if is_editable?(state) do
+  defp render_update_buttons(item, order) do
+    if is_editable?(order.state) do
       content_tag(
         :td,
-        content_tag(
-          :button,
-          [
-            "edit ",
-            content_tag(:span, tag(:i, class: "far fa-edit"), class: "badge badge-light")
-          ],
-          class: "btn btn-primary",
-          onclick: "foo()"
-        )
+        form_tag("/orders/#{order.number}/cart/edit?update=#{item}", method: "post") do
+          content_tag(:button, ["update"], class: "btn btn-primary", type: "submit")
+        end
+      )
+    end
+  end
+
+  defp render_buttons(item, order) do
+    if is_editable?(order.state) do
+      content_tag(
+        :td,
+        form_tag("/orders/#{order.number}/cart?edit=#{item}", method: "post") do
+          content_tag(:button, ["remove"], class: "btn btn-primary", type: "submit")
+        end
       )
     end
   end
@@ -108,4 +116,61 @@ defmodule AdminAppWeb.OrderView do
   end
 
   defp is_editable?(_), do: true
+
+  defp render_search_item(item, order) do
+    content = [
+      content_tag(:td, item.sku),
+      content_tag(:td, item.selling_price),
+      content_tag(:td, tag(:input, name: "quantity", id: "quantity")),
+      content_tag(:td, content_tag(:button, ["Add"], type: "submit"))
+    ]
+
+    list =
+      form_tag("/orders/#{order.number}/cart?add=#{item.id}", method: "put") do
+        List.flatten(content)
+      end
+
+    content_tag(:tr, list)
+  end
+
+  def render_update_item(item, order) do
+    content = [
+      content_tag(:td, item.variant.sku),
+      content_tag(:td, item.variant.selling_price),
+      content_tag(:td, tag(:input, name: "quantity", value: item.quantity)),
+      content_tag(:td, tag(:hidden, name: "variant_id", value: item.variant_id)),
+      content_tag(:td, content_tag(:button, ["Add"], type: "submit"))
+    ]
+
+    list =
+      form_tag("/orders/#{order.number}/cart/update?update=#{item.id}", method: "put") do
+        List.flatten(content)
+      end
+
+    content_tag(:tr, list)
+  end
+
+  def build_address(address, order) do
+    content = [
+      content_tag(:td, address.first_name),
+      content_tag(:td, address.last_name),
+      content_tag(:td, address.address_line_1),
+      content_tag(:td, address.phone),
+      content_tag(:td, address.city),
+      content_tag(
+        :td,
+        content_tag(:button, ["Attach"], type: "submit", class: "btn btn-sm btn-primary")
+      )
+    ]
+
+    list =
+      form_tag(
+        "/orders/#{order.number}/address/search?address_id=#{address.id}",
+        method: "put"
+      ) do
+        List.flatten(content)
+      end
+
+    content_tag(:tr, list)
+  end
 end
