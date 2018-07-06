344cb4458edadc43af26b7005259f164258ad3c9
blackode <codeme@gmx.com>
2018-07-06 06:43:16 +0530

    Fix Merge conflicts

--- apps/snitch_api/lib/snitch_api_web/controllers/product_controller.ex
+++ apps/snitch_api/lib/snitch_api_web/controllers/product_controller.ex
@@ -6,19 +6,6 @@ defmodule SnitchApiWeb.ProductController do
 
   import Ecto.Query, only: [from: 2]
 
-  def index(conn, params) do
-    {page_number, size} = extract_page_params(params)
-
-    query = from(p in Product, select: p)
-
-    page =
-      query
-      |> Repo.all()
-      |> gen_page_links(params, conn)
-
-<<<<<<< HEAD
-  import Ecto.Query, only: [from: 2]
-
   # sort the products in Z-A descending order_by prodcut name
   def index(conn, %{"sort" => "Z-A"} = params) do
     query = from(p in Product, order_by: [desc: p.name])
@@ -49,22 +36,15 @@ defmodule SnitchApiWeb.ProductController do
     render_view(query, params, conn)
   end
 
-  def show(conn, %{"product_slug" => slug} = params) do
+  def show(conn, %{"product_slug" => slug}) do
     product =
       Product
       |> Repo.get_by(slug: slug)
-=======
-    products =
-      query
-      |> paginate(page_number, size)
-      |> Repo.all()
->>>>>>> 53ea4bc... Products list pagination url links
       |> Repo.preload(variants: [:images])
 
     render(
       conn,
-      "index.json-api",
-<<<<<<< HEAD
+      "show.json-api",
       data: product,
       opts: [include: "variants,variants.images"]
     )
@@ -79,51 +59,20 @@ defmodule SnitchApiWeb.ProductController do
       |> gen_page_links(params, conn)
 
     products =
-      Repo.all(Product)
-=======
-      data: products,
-      opts: [include: "variants,variants.images", page: page]
-    )
-  end
-
-  def show(conn, %{"product_slug" => slug} = params) do
-    IO.inspect(params, label: "............inside show....")
-
-    product =
-      Product
-      |> Repo.get_by(slug: slug)
->>>>>>> 53ea4bc... Products list pagination url links
+      query
+      |> paginate(page_number, size)
+      |> Repo.all()
       |> Repo.preload(variants: [:images])
 
     render(
       conn,
       "index.json-api",
-<<<<<<< HEAD
       data: products,
-      opts: [include: "variants"]
+      opts: [include: "variants,variants.images", page: page]
     )
   end
 
   defp gen_page_links(collection, params, conn) do
-    {number, size} = extract_page_params(params)
-
-    JaSerializer.Builder.PaginationLinks.build(
-      %{
-        number: number,
-        size: size,
-        total: div(Enum.count(collection), size),
-        base_url: "http://0.0.0.0:3000/api/v1/products"
-      },
-      conn
-    )
-  end
-=======
-      data: product,
-      opts: [include: "variants,variants.images"]
-    )
-  end
-
-  def gen_page_links(collection, params, conn) do
     {number, size} = extract_page_params(params)
 
     JaSerializer.Builder.PaginationLinks.build(
@@ -154,5 +103,4 @@ defmodule SnitchApiWeb.ProductController do
         {1, Application.get_env(:ja_serializer, :page_size, 2)}
     end
   end
->>>>>>> 53ea4bc... Products list pagination url links
 end
