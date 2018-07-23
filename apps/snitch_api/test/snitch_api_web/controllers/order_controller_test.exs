e3f9f13a10e2ee4361eb47d81ca85906b5457c3b
Anwesh <kanishkablack@users.noreply.github.com>
2018-07-23 11:59:50 +0530

    Api user orders (#145)
    API Order
    --
    - Adds listing for user's orders
    - only accessible to authorised user.

--- apps/snitch_api/test/snitch_api_web/controllers/order_controller_test.exs
+++ apps/snitch_api/test/snitch_api_web/controllers/order_controller_test.exs
@@ -1,18 +1,43 @@
 defmodule SnitchApiWeb.OrderControllerTest do
   use SnitchApiWeb.ConnCase, async: true
+  import Snitch.Factory
+
+  alias SnitchApi.Accounts
+  alias Snitch.Repo
   alias Snitch.Data.Model.Order, as: OrderModel
 
   setup %{conn: conn} do
+    insert(:role, name: "user")
+    user = build(:user_with_no_role)
+
     conn =
       conn
       |> put_req_header("accept", "application/vnd.api+json")
       |> put_req_header("content-type", "application/vnd.api+json")
 
-    {:ok, conn: conn}
+    {:ok, registered_user} = Accounts.create_user(user)
+    {:ok, token, _claims} = SnitchApi.Guardian.encode_and_sign(registered_user)
+    {:ok, conn: conn, token: token}
   end
 
   test "Empty order creation for guest user", %{conn: conn} do
     conn = post(conn, order_path(conn, :guest_order))
     assert json_response(conn, 200)["data"]
+  end
+
+  describe "user orders" do
+    test "unauthenticated user", %{conn: conn} do
+      resp = get(conn, order_path(conn, :index))
+      assert resp.status == 403
+      resp_body = Jason.decode!(resp.resp_body)
+      assert resp_body["error"] == "unauthenticated"
+    end
+
+    test "Auth user", %{conn: conn, token: token} do
+      conn = conn |> put_req_header("authorization", "Bearer #{token}")
+      resp = get(conn, order_path(conn, :index))
+      resp_data = Jason.decode!(resp.resp_body)
+      assert [] == resp_data["data"]
+    end
   end
 end
