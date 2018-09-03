8edab12ca07a5f7bcf459b01759c93b28a2de7fa
Sagar Karwande <sagar_karwande123@yahoo.com>
2018-09-04 00:16:32 +0530

    Add api for brands (#168)
    api_app
    ---------
    - add API for listing brands
    - update products API to return products based on brands
    
    remove unecessary preloads
    
    load rating for product
    
    update jwt expire time to 3 days
    
    minor fixes

---
+++ apps/snitch_api/lib/snitch_api_web/controllers/product_brand_controller.ex
@@ -0,0 +1,18 @@
+defmodule SnitchApiWeb.ProductBrandController do
+  use SnitchApiWeb, :controller
+
+  alias SnitchApiWeb.ProductBrandView
+  alias Snitch.Data.Model.ProductBrand
+  alias Snitch.Repo
+
+  def index(conn, _params) do
+    brands = ProductBrand.get_all() |> Repo.preload(:image)
+
+    render(
+      conn,
+      ProductBrandView,
+      "index.json-api",
+      data: brands
+    )
+  end
+end
