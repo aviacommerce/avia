f2d43888e9cb52111c1b16500c4ca288f82100c1
Sagar Karwande <sagar_karwande123@yahoo.com>
2018-09-14 14:54:35 +0530

    add aws s3 image upload support (#189)

--- apps/snitch_core/config/config.exs
+++ apps/snitch_core/config/config.exs
@@ -3,4 +3,15 @@ use Mix.Config
 config :snitch_core, ecto_repos: [Snitch.Repo]
 config :ecto, :json_library, Jason
 
+config :arc,
+  bucket: {:system, "BUCKET_NAME"},
+  virtual_host: true
+
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
+  s3: [
+    region: {:system, "AWS_REGION"}
+  ]
+
 import_config "#{Mix.env()}.exs"
