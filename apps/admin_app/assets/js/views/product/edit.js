4b73f1a936d10ee93bf5b33b2cb0e02f8f1094cf
jaypal412 <jaypal@aviabird.com>
2018-09-17 09:27:26 +0530

    Fix UI for product edit and login page (#194)

--- apps/admin_app/assets/js/views/product/edit.js
+++ apps/admin_app/assets/js/views/product/edit.js
@@ -1,22 +1,22 @@
 import MainView from '../main';
 
 export default class View extends MainView {
-    mount() {
-      super.mount();
+  mount() {
+    super.mount();
 
-      // Specific logic here
-      console.log('ProductEditView mounted');
-      handleImageSelect();
-      deleteImage();
-      setup_product();
-    }
+    // Specific logic here
+    console.log('ProductEditView mounted');
+    handleImageSelect();
+    deleteImage();
+    setup_product();
+  }
 
-    unmount() {
-      super.unmount();
+  unmount() {
+    super.unmount();
 
-      // Specific logic here
-      console.log('ProductEditView unmounted');
-    }
+    // Specific logic here
+    console.log('ProductEditView unmounted');
+  }
 }
 
 var selDiv
@@ -28,20 +28,20 @@ export function handleImageSelect() {
 }
 
 function selectedFile(e) {
-  if(!e.target.files || !window.FileReader) return;
+  if (!e.target.files || !window.FileReader) return;
 
   selDiv.empty()
   var files = e.target.files;
   let filesArr = Array.prototype.slice.call(files);
 
-  filesArr.forEach(function(f) {
-    if(!f.type.match("image.*")) {
+  filesArr.forEach(function (f) {
+    if (!f.type.match("image.*")) {
       return;
     }
     storedFile.push(f)
     var reader = new FileReader();
     reader.onload = function (e) {
-      var html = "<img src=\"" + e.target.result + "\">" + f.name + "<br clear=\"left\"/>";
+      var html = "<div class=\"col-6 px-1 mb-2\"><div class=\"media\"><img class=\"align-self-start mr-3\" src=\"" + e.target.result + "\" alt=\"Generic placeholder image\" /> <div class=\"media-body\"><h5 class=\"mt-0\">" + f.name + "</h5> </div> </div></div>";
       selDiv.append(html);
     }
     reader.readAsDataURL(f);
@@ -49,7 +49,7 @@ function selectedFile(e) {
 }
 
 export function deleteImage() {
-  $(".product-delete").click(function() {
+  $(".product-delete").click(function () {
     let product_id = $("#product-id").val();
     let image_id = $(this).find("input").val()
     delete_product_image(product_id, image_id, this)
@@ -65,7 +65,7 @@ function delete_product_image(product_id, image_id, reference) {
       image_id: image_id
     }),
     type: 'DELETE',
-    beforeSend: function(xhr) {
+    beforeSend: function (xhr) {
       xhr.setRequestHeader("X-CSRF-Token", CSRF_TOKEN);
     },
     success: function (data) {
@@ -84,74 +84,74 @@ function deleteError(data) {
   console.log('errror data', data)
 }
 
-function setup_product(){
-    let previousOptionValue = ""
+function setup_product() {
+  let previousOptionValue = ""
 
-    // This handle the variation theme selection
-    $('#product_theme_id').on('change', function (e) {
-      $('#theme_change_modal').modal(`show`);
-    })
+  // This handle the variation theme selection
+  $('#product_theme_id').on('change', function (e) {
+    $('#theme_change_modal').modal(`show`);
+  })
 
-    $("#theme_change_confirm").click(function(e){
-        var optionSelected = $('#product_theme_id');
-        var valueSelected = optionSelected.val();
-        const product_id = $(this)
-          .parents()
-          .find('#product_id');
-        const new_variant = $(this)
-          .parents()
-          .find('#new_variant');
-        var link = "/products/" + product_id.val() + "/variant/new?theme_id=" + valueSelected
-        new_variant.attr("href", link)
+  $("#theme_change_confirm").click(function (e) {
+    var optionSelected = $('#product_theme_id');
+    var valueSelected = optionSelected.val();
+    const product_id = $(this)
+      .parents()
+      .find('#product_id');
+    const new_variant = $(this)
+      .parents()
+      .find('#new_variant');
+    var link = "/products/" + product_id.val() + "/variant/new?theme_id=" + valueSelected
+    new_variant.attr("href", link)
 
-        get_variation_options(valueSelected, product_id.val())
-    })
+    get_variation_options(valueSelected, product_id.val())
+  })
 
-    $(".option-value")
-    .blur(function(evt){
+  $(".option-value")
+    .blur(function (evt) {
       var td = $(evt.target)
       let option_value_id = td.data("option_value_id")
       let value = td[0].innerText
-      if(value !== "")
+      if (value !== "")
         update_option_value(option_value_id, value)
       else {
         td.text(previousOptionValue)
       }
     })
 
-    $(".option-value")
-    .focusin(function(evt){
+  $(".option-value")
+    .focusin(function (evt) {
       var td = $(evt.target)
       previousOptionValue = td[0].innerText
     })
 
-    var product_id = $("#product_id").val();
-    var theme_id = $("#product_theme_id").val();
-    if(theme_id !== undefined && !(theme_id == ""))
-      get_variation_options(theme_id, product_id)
+  var product_id = $("#product_id").val();
+  var theme_id = $("#product_theme_id").val();
+  if (theme_id !== undefined && !(theme_id == ""))
+    get_variation_options(theme_id, product_id)
 }
 
-function update_option_value(id, value){
-    $.ajax({
-      type: "POST",
-      url: 'http://localhost:3000/api/v1/product_option_values/' + id,
-      crossDomain: true,
-      data: {value: value},
-      success: function(response){
-      }
-    })
-  }
+function update_option_value(id, value) {
+  $.ajax({
+    type: "POST",
+    url: 'http://localhost:3000/api/v1/product_option_values/' + id,
+    crossDomain: true,
+    data: {
+      value: value
+    },
+    success: function (response) {}
+  })
+}
 
-  function get_variation_options(theme_id, product_id)
-  {
-    fetch('http://localhost:4000/api/option_types?theme_id=' + theme_id + "&product_id=" + product_id)
-    .then(function(response) {
+function get_variation_options(theme_id, product_id) {
+  fetch('http://localhost:4000/api/option_types?theme_id=' + theme_id + "&product_id=" + product_id)
+    .then(function (response) {
       return response.json();
     })
-    .then(function(myJson) {
+    .then(function (myJson) {
       $('#variation_options')
-      .empty()
-      .append(myJson.html)
+        .empty()
+        .append(myJson.html)
     });
-    $('#theme_change_modal').modal('hide');
-  }
+  $('#theme_change_modal').modal('hide');
+}
