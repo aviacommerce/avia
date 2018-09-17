138355bd7711c42daf51ddb1bd077d960be21904
JyotiGautam <jyotigautam108@gmail.com>
2018-09-17 19:24:30 +0530

    Fixing arc config.(#196)

--- apps/snitch_core/config/prod.exs
+++ apps/snitch_core/config/prod.exs
@@ -4,3 +4,4 @@ import_config "prod.secret.exs"
 
 config :snitch_core, :defaults_module, Snitch.Tools.Defaults
 config :snitch_core, :user_config_module, Snitch.Tools.UserConfig
+config :arc, storage: Arc.Storage.S3
