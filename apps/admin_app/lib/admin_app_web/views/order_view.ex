0f4089a0fa4617f1512a0298d48de0fb7c62bbcf
anantanant2015 <anantanant2015@gmail.com>
2018-09-23 00:28:24 +0530

    To add invoice, packaging slip generation feature for order using html2canvas, jsPDF.(#207)
    Prior to this change,
    The pdf generation feature was implemented using pdf generator
    library, which is resulting in compilation error in production.
    
    This change
    - Updates packing slip, invoice template of order
    - Updates app html for scripts required to generate pdf
    - Removes config for pdf generator

--- apps/admin_app/lib/admin_app_web/views/order_view.ex
+++ apps/admin_app/lib/admin_app_web/views/order_view.ex
@@ -195,45 +195,18 @@ defmodule AdminAppWeb.OrderView do
   end
 
   def render_invoice_links(order) do
-    order_new =
-      order
-      |> Repo.preload(:line_items)
-      |> Repo.preload(:user)
-
-    html = Phoenix.View.render_to_string(AdminAppWeb.OrderView, "invoice.html", order: order_new)
-
-    payslip_html =
-      Phoenix.View.render_to_string(AdminAppWeb.OrderView, "packing_slip.html", order: order_new)
-
-    {:ok, file} = PdfGenerator.generate_binary(html, page_size: "A4", delete_temporary: false)
-
-    {:ok, packing_slip_file} =
-      PdfGenerator.generate_binary(payslip_html, page_size: "A4", delete_temporary: false)
-
-    path = "invoices/#{order.number}.pdf"
-    packing_slip_path = "invoices/packing_slip_#{order.number}.pdf"
-    File.write(path, file)
-    File.write(packing_slip_path, packing_slip_file)
-
     content = [
       content_tag(
         :a,
-        "Download Invoice",
-        href: "#{order.number}/download-invoice",
+        "Show Invoice",
+        href: "show-invoice",
         class: "btn btn-primary",
         style: "margin-right: 3px;"
       ),
       content_tag(
         :a,
         "Show Packing Slip",
-        href: "#{order.number}/show-packing-slip",
-        class: "btn btn-primary",
-        style: "margin-right: 3px;"
-      ),
-      content_tag(
-        :a,
-        "Download Packing Slip",
-        href: "#{order.number}/download-packing-slip",
+        href: "show-packing-slip",
         class: "btn btn-primary",
         style: "margin-right: 3px;"
       )
