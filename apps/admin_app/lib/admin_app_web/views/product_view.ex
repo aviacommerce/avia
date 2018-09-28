04145d2d89d0953353be556f4b4b6dc0af4828e3
JyotiGautam <jyotigautam108@gmail.com>
2018-09-28 12:47:25 +0530

    Fixes variation tab not hiding issue for child product (#219)

--- apps/admin_app/lib/admin_app_web/views/product_view.ex
+++ apps/admin_app/lib/admin_app_web/views/product_view.ex
@@ -1,6 +1,8 @@
 defmodule AdminAppWeb.ProductView do
   use AdminAppWeb, :view
   alias Snitch.Data.Model.Product
+  alias Snitch.Data.Schema.Variation
+  alias Snitch.Repo
   import Ecto.Query
 
   @currencies ["USD", "INR"]
@@ -10,34 +12,44 @@ defmodule AdminAppWeb.ProductView do
     image.url
   end
 
-  def themes_options(parent_product) do
-    Enum.map(parent_product.taxon.variation_themes, fn theme -> {theme.name, theme.id} end)
+  def themes_options(product) do
+    Enum.map(product.taxon.variation_themes, fn theme -> {theme.name, theme.id} end)
   end
 
   # TODO This needs to be replaced and we need a better system to identify
   # the type of product.
   def is_parent_product(product_id) when is_binary(product_id) do
-    q =
+    query =
       from(
         p in "snitch_product_variants",
         where: p.parent_product_id == ^(product_id |> String.to_integer()),
         select: fragment("count(*)")
       )
 
-    count = Snitch.Repo.one(q)
+    count = Snitch.Repo.one(query)
     count > 0
   end
 
-  def has_themes(parent_product) do
-    length(parent_product.taxon.variation_themes) > 0
+  def can_add_variant(product) do
+    has_themes(product) && !is_child_product(product)
   end
 
-  def has_variants(parent_product) do
-    parent_product.variants |> length > 0
+  def has_themes(product) do
+    length(product.taxon.variation_themes) > 0
   end
 
-  def get_option_types(parent_product) do
-    variant = parent_product.variants |> List.first()
+  defp is_child_product(product) do
+    query = from(c in Variation, where: c.child_product_id == ^product.id)
+    count = Repo.aggregate(query, :count, :id)
+    count > 0
+  end
+
+  def has_variants(product) do
+    product.variants |> length > 0
+  end
+
+  def get_option_types(product) do
+    variant = product.variants |> List.first()
 
     variant.options
     |> Enum.map(fn x -> x.option_type end)
