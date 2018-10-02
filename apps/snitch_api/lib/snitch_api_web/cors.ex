7fd43f8b5b2e54299d1e050aa17e5497b16b3f1a
Sagar Karwande <sagar_karwande123@yahoo.com>
2018-10-02 23:42:32 +0530

    Allow all origin for API calls

--- apps/snitch_api/lib/snitch_api_web/cors.ex
+++ apps/snitch_api/lib/snitch_api_web/cors.ex
@@ -2,7 +2,7 @@ defmodule ApiWeb.CORS do
   @moduledoc false
 
   use Corsica.Router,
-    origins: ["http://localhost:4200", "http://localhost:4000"],
+    origins: "*",
     log: [rejected: :error],
     allow_credentials: true,
     allow_headers: ["content-type", "token-type", "authorization"],
