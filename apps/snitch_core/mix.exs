fb1b5cebb4b381bbdbb1c71b920149c7a0c97155
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-10-03 18:05:45 +0530

    updating travis build fail with updated eixir version

--- apps/snitch_core/mix.exs
+++ apps/snitch_core/mix.exs
@@ -11,7 +11,7 @@ defmodule Snitch.Core.Mixfile do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: ">= 1.5.3",
+      elixir: "~> 1.7.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
