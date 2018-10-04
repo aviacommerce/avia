e1f339f4419fe239c270d09bc8646209f2e36f91
JyotiGautam <jyotigautam108@gmail.com>
2018-10-04 10:40:49 +0530

    fixing order confirmation mail template

--- apps/snitch_core/mix.exs
+++ apps/snitch_core/mix.exs
@@ -43,7 +43,6 @@ defmodule Snitch.Core.Mixfile do
     [
       {:postgrex, "~> 0.13.5"},
       {:ecto, "~> 2.2"},
-      {:jason, "~> 1.0"},
       {:bamboo, "~> 1.1"},
       {:bamboo_smtp, "~> 1.6.0"},
       {:bamboo_eex, "~> 0.1.0"},
