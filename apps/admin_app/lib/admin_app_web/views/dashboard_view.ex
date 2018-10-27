65bdcc9ce452f96a20f33cd426a646376b0ac3de
Jyoti Gautam <jyotigautam108@gmail.com>
2018-10-27 19:06:42 +0530

    Redirect to dashboard after login (#295)
    * Redirect to dashboard after login
    * Change default start date for graph to one month from today.

--- apps/admin_app/lib/admin_app_web/views/dashboard_view.ex
+++ apps/admin_app/lib/admin_app_web/views/dashboard_view.ex
@@ -4,6 +4,22 @@ defmodule AdminAppWeb.DashboardView do
   alias AdminAppWeb.Helpers
 
   def get_date(params, key) do
+    get_time(params, key, params[key])
+  end
+
+  defp get_time(params, "from", nil) do
+    start_time = Ecto.DateTime.utc()
+
+    %{start_time | month: start_time.month - 1}
+    |> Ecto.DateTime.to_date()
+    |> to_string
+  end
+
+  defp get_time(params, "from", param_key) do
+    Helpers.get_date_from_params(params, "from")
+  end
+
+  defp get_time(params, key, param_key) do
     Helpers.get_date_from_params(params, key)
   end
 end
