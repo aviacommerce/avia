d157efa5371da0be6cdd4e59948a1b317461ea7f
anantanant2015 <anantanant2015@gmail.com>
2018-11-17 11:42:52 +0530

    admin_app ---------
    - Adds promotion edit page toggle to buttons, and rule, module
      list updates with user actions.

--- apps/admin_app/assets/js/views/promotion/edit.js
+++ apps/admin_app/assets/js/views/promotion/edit.js
@@ -10,6 +10,7 @@ export default class View extends MainView {
         var CSRF_TOKEN = $("meta[name='csrf-token']").attr("content");
         let target_div = $("#rule_module_content");
         let rule_module = document.getElementById("rule_module");
+
         let form_type = {option: rule_module.value};
         $.ajax({
           url: `${window.location.pathname}/rule/render-form`,
@@ -20,6 +21,8 @@ export default class View extends MainView {
           data: form_type,
           success: function(data){
             target_div.append(data.html);
+            toggle_element("rule_module_submit_btn");
+            remove_rule_module_option(rule_module.selectedIndex);
           }
         });
       }
@@ -31,6 +34,19 @@ export default class View extends MainView {
         input_form.method = "post";
         document.getElementById("input_form").submit();          
       }
+
+      toggle_element("rule_module_submit_btn");
+
+      document.getElementById("input_form").onchange = function() {
+        toggle_element("rule_module_submit_btn");
+      }
+
+      document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+          var rule_module_list = get_existing_promotion_rule_modules();
+          remove_rule_module_option_from(rule_module_list);
+      }
+    }
       }
 
       
@@ -40,4 +56,52 @@ export default class View extends MainView {
       // Specific logic here
       console.log('PromotionEditView unmounted');
     }
+}
+
+
+export function toggle_element(id) {
+  var x = document.getElementById(id);
+  if (x.disabled === true) {
+    x.disabled = false;
+  } else {
+    x.disabled = true;
+  }
+}
+
+export function remove_rule_module_option(index) {
+  let rule_module = document.getElementById("rule_module");
+  rule_module.remove(index);
+  if (rule_module.length == 0) {
+    toggle_element("rule_module_add_btn");
+    toggle_element("rule_module");
+  }
+}
+
+export function remove_rule_module_option_from(rule_module_list) {
+  let rule_module = document.getElementById("rule_module");
+  var i;
+  var len = rule_module.length;
+  let index_list = [];
+  for(i=0; i < len; i++) {
+    if (rule_module_list.indexOf(rule_module[i].value) != -1) {
+      // remove_rule_module_option(i);
+      index_list.push(i);
+    }
+  }
+
+  for(i=0; i < index_list.length; i++) {
+    remove_rule_module_option(index_list[i]);
+  }
+}
+
+export function get_existing_promotion_rule_modules() {
+  let rule_module_names = document.getElementsByClassName("rule_module_name");
+
+  var i;
+  let module_list = [];
+  for(i=0; i < rule_module_names.length; i++) {
+    module_list.push(rule_module_names[i].value);
+  }
+
+  return module_list;
 }
