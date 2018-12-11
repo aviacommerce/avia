0b4f0437736eae15cc54222d81213162bb4c5338
JyotiGautam <jyotigautam108@gmail.com>
2018-12-11 14:25:40 +0530

    conflict fix

--- apps/snitch_core/priv/repo/demo/stock_locations.ex
+++ apps/snitch_core/priv/repo/demo/stock_locations.ex
@@ -10,88 +10,73 @@ defmodule Snitch.Demo.StockLocation do
     state = get_random_state
     product_path = Path.join(@base_path, "stock_locations.csv")
 
-    def create_stock_locations do
-      Repo.delete_all(StockLocation)
-      state = get_random_state
-      product_path = Path.join(@base_path, "stock_locations.csv")
+    product_path
+    |> File.read!()
+    |> CSV.parse_string()
+    |> Enum.each(fn [
+                      name,
+                      default,
+                      address_line_1,
+                      address_line_2,
+                      city,
+                      zip_code,
+                      phone,
+                      propagate_all_variants,
+                      active
+                    ] ->
+      default = String.to_existing_atom(default)
+      active = String.to_existing_atom(active)
+      propagate_all_variants = String.to_existing_atom(propagate_all_variants)
 
-      product_path
-      |> File.read!()
-      |> CSV.parse_string()
-      |> Enum.each(fn [
-                        name,
-                        default,
-                        address_line_1,
-                        address_line_2,
-                        city,
-                        zip_code,
-                        phone,
-                        propagate_all_variants,
-                        active
-                      ] ->
-        default = String.to_existing_atom(default)
-        active = String.to_existing_atom(active)
-        propagate_all_variants = String.to_existing_atom(propagate_all_variants)
+      create_stock_location!(
+        name,
+        default,
+        address_line_1,
+        address_line_2,
+        city,
+        zip_code,
+        phone,
+        propagate_all_variants,
+        active,
+        state,
+        state.country
+      )
+    end)
+  end
 
-        create_stock_location!(
-          name,
-          default,
-          address_line_1,
-          address_line_2,
-          city,
-          zip_code,
-          phone,
-          propagate_all_variants,
-          active,
-          state,
-          state.country
-        )
-      end)
-    end
+  defp get_random_state do
+    State
+    |> Repo.all()
+    |> Repo.preload([:country])
+    |> Enum.random()
+  end
 
-    defp get_random_state do
-      State
-      |> Repo.all()
-      |> Repo.preload([:country])
-      |> Enum.random()
-    end
-
-    defp get_random_state do
-      State
-      |> Repo.all()
-      |> Repo.preload([:country])
-      |> Enum.random()
-    end
-
-    def create_stock_location!(
-          name,
-          default,
-          address_line_1,
-          address_line_2,
-          city,
-          zip_code,
-          phone,
-          propagate_all_variants,
-          active,
-          state,
-          country
-        ) do
-      params = %{
-        name: name,
-        default: default,
-        address_line_1: address_line_1,
-        address_line_2: address_line_2,
-        city: city,
-        zip_code: zip_code,
-        phone: phone,
-        propagate_all_variants: propagate_all_variants,
-        active: active,
-        state_id: state.id,
-        country_id: country.id
-      }
-
-      %StockLocation{} |> StockLocation.create_changeset(params) |> Repo.insert!()
-    end
+  def create_stock_location!(
+        name,
+        default,
+        address_line_1,
+        address_line_2,
+        city,
+        zip_code,
+        phone,
+        propagate_all_variants,
+        active,
+        state,
+        country
+      ) do
+    params = %{
+      name: name,
+      default: default,
+      address_line_1: address_line_1,
+      address_line_2: address_line_2,
+      city: city,
+      zip_code: zip_code,
+      phone: phone,
+      propagate_all_variants: propagate_all_variants,
+      active: active,
+      state_id: state.id,
+      country_id: country.id
+    }
 
     %StockLocation{} |> StockLocation.create_changeset(params) |> Repo.insert!()
   end
