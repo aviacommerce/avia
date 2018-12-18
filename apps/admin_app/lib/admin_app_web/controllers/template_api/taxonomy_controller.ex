3018c49377d98c5796e82bc7d9116db074e1944d
Sagar Karwande <sagar_karwande123@yahoo.com>
2018-12-18 10:17:59 +0530

    minor fixes

--- apps/admin_app/lib/admin_app_web/controllers/template_api/taxonomy_controller.ex
+++ apps/admin_app/lib/admin_app_web/controllers/template_api/taxonomy_controller.ex
@@ -9,7 +9,7 @@ defmodule AdminAppWeb.TemplateApi.TaxonomyController do
   import Phoenix.View, only: [render_to_string: 3]
 
   def index(conn, %{"taxon_id" => taxon_id}) do
-    categories = Taxonomy.get_child_taxons(taxon_id)
+    {:ok, categories} = Taxonomy.get_child_taxons(taxon_id)
 
     if(length(categories) > 0) do
       html = render_to_string(TaxonomyView, "taxons.html", taxons: categories)
