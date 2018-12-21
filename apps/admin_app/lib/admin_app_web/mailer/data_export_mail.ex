84e4ab393a966b23449576c17e30a6a245af7bd7
JyotiGautam <jyotigautam108@gmail.com>
2018-12-21 18:08:48 +0530

    user email added

--- apps/admin_app/lib/admin_app_web/mailer/data_export_mail.ex
+++ apps/admin_app/lib/admin_app_web/mailer/data_export_mail.ex
@@ -10,7 +10,7 @@ defmodule AdminAppWeb.DataExportMail do
     sender_email = Application.get_env(:admin_app, AdminAppWeb.Endpoint)[:sendgrid_sender_mail]
 
     new()
-    |> to("jyotigautam108@gmail.com")
+    |> to(user.email)
     |> from({"Aviacommerce", sender_email})
     |> subject("Your #{type}s")
     |> text_body("Here is the #{format} export of your #{type}s.")
