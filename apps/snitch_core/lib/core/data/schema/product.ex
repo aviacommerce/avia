4c538c4a458dd8ff9d73821aa4ccf52cf6984872
Sagar Karwande <sagar_karwande123@yahoo.com>
2018-12-27 15:28:32 +0530

    add inventory tab

--- apps/snitch_core/lib/core/data/schema/product.ex
+++ apps/snitch_core/lib/core/data/schema/product.ex
@@ -86,7 +86,7 @@ defmodule Snitch.Data.Schema.Product do
   end
 
   @required_fields ~w(name selling_price max_retail_price taxon_id shipping_category_id)a
-  @optional_fields ~w(description meta_description meta_keywords meta_title brand_id height width depth weight state)a
+  @optional_fields ~w(description meta_description meta_keywords meta_title brand_id height width depth weight state inventory_tracking)a
 
   def create_changeset(model, params \\ %{}) do
     common_changeset(model, params)
