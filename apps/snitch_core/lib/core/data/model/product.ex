efc42fb832c429613610c87b1fc832824ba0afd5
anantanant2015 <anantanant2015@gmail.com>
2018-12-31 12:40:36 +0530

    Dashboard page state count and date.
    Why?
    - Dashboard page should display all products state count excluding
      date inputs from user.
    - Dashboard page with no query params for date should display from
      date in the page.
    
    This change addresses the need by:
    - Updating product method to generate state count, for not using
      user inputs for date.
    - Updating dashboard controller to send date as params to template.
    
    [finishes #162891353]

--- apps/snitch_core/lib/core/data/model/product.ex
+++ apps/snitch_core/lib/core/data/model/product.ex
@@ -349,7 +349,7 @@ defmodule Snitch.Data.Model.Product do
     Enum.reduce(stocks, 0, fn stock, acc -> stock.count_on_hand + acc end)
   end
 
-  def get_product_count_by_state(start_date, end_date) do
+  def get_product_count_by_state() do
     child_product_ids =
       Variation
       |> select([v], v.child_product_id)
@@ -358,8 +358,7 @@ defmodule Snitch.Data.Model.Product do
     Product
     |> where(
       [p],
-      p.inserted_at >= ^start_date and p.inserted_at <= ^end_date and p.state in ^@product_states and
-        p.id not in ^child_product_ids
+      p.state in ^@product_states and p.id not in ^child_product_ids
     )
     |> group_by([p], p.state)
     |> select([p], %{state: p.state, count: count(p.id)})
