f2b04c3d9272aec6e57342275eb24229021bf6cb
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2018-12-31 22:23:52 +0530

    [#162446059] Moved docker configuration files to config/docker/dev folder

--- config/docker/dev/docker-dev-provision.sh
+++ config/docker/dev/docker-dev-provision.sh
@@ -4,3 +4,4 @@ mix ecto.migrate
 mix run apps/snitch_core/priv/repo/seed/seeds.exs
 mix cmd --app snitch_core mix elasticsearch.build products --cluster Snitch.Tools.ElasticsearchCluster
 mix phx.server
+
