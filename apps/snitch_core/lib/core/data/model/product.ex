f6d48720fc1beb68a4ca8f23874206a65123b9af
JyotiGautam <jyotigautam108@gmail.com>
2019-01-03 17:29:00 +0530

    handle image deletion according to tenant

--- apps/snitch_core/lib/core/data/model/product.ex
+++ apps/snitch_core/lib/core/data/model/product.ex
@@ -174,6 +174,8 @@ defmodule Snitch.Data.Model.Product do
   def delete(id) do
     with %Product{} = product <- get(id),
          changeset <- Product.delete_changeset(product) do
+      product = product |> Repo.preload(:images)
+      Enum.map(product.images, &delete_image(product.id, &1.id))
       Repo.update(changeset)
     end
   end
