b7ea8228d0235f6a2bfb40609e78b15bb1384a28
Pankaj Kumar Rawat <pankajrawat19sept@gmail.com>
2019-01-10 18:03:46 +0530

    Caching Taxonomy and search responses as per the url (#419)
    - Adds a cache tool in snitch_core which can be called from anywhere to retrieve the cached result.
    - Added cache support to taxonomy api, product listing, product search autocompletion.

---
+++ apps/snitch_core/lib/core/tools/cache.ex
@@ -0,0 +1,24 @@
+defmodule Snitch.Tools.Cache do
+  @moduledoc """
+  Helper for caching data
+  """
+
+  @cache_name :avia_cache
+
+  @doc """
+  Fetches the data
+  * from cache if exists.
+  * from the callback function `fun` returning the data
+  """
+  def get(cache_key, {fun, args}, expiry \\ :timer.hours(2)) do
+    with {:ok, value} <- Cachex.get(@cache_name, cache_key),
+           false <- is_nil(value) do
+        value
+      else
+        _ ->
+          result = apply(fun, args)
+          Cachex.put(@cache_name, cache_key, result, ttl: expiry)
+          result
+      end
+  end
+end
