e0e02be7155def7093512111b53b63dfd059d0fb
Pankaj Rawat <pankajrawat19sept@gmail.com>
2019-01-10 16:47:42 +0530

    refactored caching code

--- apps/snitch_api/lib/snitch_api_web/controllers/taxonomy_controller.ex
+++ apps/snitch_api/lib/snitch_api_web/controllers/taxonomy_controller.ex
@@ -5,27 +5,21 @@ defmodule SnitchApiWeb.TaxonomyController do
   alias Snitch.Core.Tools.MultiTenancy.Repo
 
   alias Snitch.Domain.Taxonomy, as: TaxonomyDomain
-  alias Cachex
-
-  @cache_name :avia_cache
+  alias Snitch.Tools.Cache
 
   action_fallback(SnitchApiWeb.FallbackController)
   plug(SnitchApiWeb.Plug.DataToAttributes)
   plug(SnitchApiWeb.Plug.LoadUser)
 
   def index(conn, _params) do
-    cache_key = Repo.get_prefix() <> "_taxonomy_list_for_api"
-
     taxonomy =
-      with {:ok, value} <- Cachex.get(@cache_name, cache_key),
-           false <- is_nil(value) do
-        value
-      else
-        _ ->
-          taxonomy = TaxonomyDomain.get_all_taxonomy()
-          Cachex.put(@cache_name, cache_key, taxonomy, ttl: :timer.hours(2))
-          taxonomy
-      end
+      Cache.get(
+        current_url(conn),
+        {
+          fn -> TaxonomyDomain.get_all_taxonomy() end,
+          []
+        }
+      )
 
     json(conn, %{taxonomies: taxonomy})
   end
