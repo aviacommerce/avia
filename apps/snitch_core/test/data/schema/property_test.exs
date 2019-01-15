2534a7ee879a11cd910a4e79df4f21126162433d
Jeshan <jeshankhn6@gmail.com>
2019-01-15 17:38:59 +0545

    [163215222] Made changes in property_test.exs

--- apps/snitch_core/test/data/schema/property_test.exs
+++ apps/snitch_core/test/data/schema/property_test.exs
@@ -6,32 +6,32 @@ defmodule Snitch.Data.Schema.PropertyTest do
 
   alias Snitch.Data.Schema.Property
 
-  @valid_attrs %{name: "Milk", display_name: "Amul milk"}
+  @valid_attrs %{name: "Sapphire Radeon", display_name: "processors"}
 
   describe "Create property" do
     test "changeset with valid attributes" do
-      %{valid?: validity} =  Property.create_changeset(%Property{},@valid_attrs)
+      %{valid?: validity} =  Property.create_changeset(%Property{}, @valid_attrs)
       assert validity
     end
 
     test "name and display_name cannot be blank" do
       params = Map.delete(@valid_attrs, :name)
-      cs = %{valid?: validity} = Property.create_changeset(%Property{},params)
+      cs = %{valid?: validity} = Property.create_changeset(%Property{}, params)
       refute validity
       assert %{name: ["can't be blank"]} = errors_on(cs)
 
       params = Map.delete(@valid_attrs, :display_name)
-      cs = %{valid?: validity} = Property.create_changeset(%Property{},params)
+      cs = %{valid?: validity} = Property.create_changeset(%Property{}, params)
       refute validity
       assert %{display_name: ["can't be blank"]} = errors_on(cs)
     end
 
     test "name must be unique" do
-      cset = Property.create_changeset(%Property{}, @valid_attrs)
-      assert {:ok, _} = Repo.insert(cset)
+      changeset = Property.create_changeset(%Property{}, @valid_attrs)
+      assert {:ok, _} = Repo.insert(changeset)
 
-      cset = Property.create_changeset(%Property{}, @valid_attrs)
-      assert {:error, cs} = Repo.insert(cset)
+      changeset = Property.create_changeset(%Property{}, @valid_attrs)
+      assert {:error, cs} = Repo.insert(changeset)
       assert %{name: ["has already been taken"]} == errors_on(cs)
     end
   end
@@ -47,31 +47,31 @@ defmodule Snitch.Data.Schema.PropertyTest do
     end
 
     test "valid attributes", %{property: property} do
-      params = %{name: "shoes",display_name: "Nike"}
-      %{valid?: validity} = Property.update_changeset(property,params)
+      params = %{name: "Corsair Power Supply", display_name: "AC input"}
+      %{valid?: validity} = Property.update_changeset(property, params)
       assert validity
     end
 
     test "name and display_name cannot be blank", %{property: property} do
-      params = %{name: "",display_name: "Nike"}
-      cs = %{valid?: validity} = Property.update_changeset(property,params)
+      params = %{name: "", display_name: "AC input"}
+      cs = %{valid?: validity} = Property.update_changeset(property, params)
       refute validity
       assert %{name: ["can't be blank"]} = errors_on(cs)
 
-      params = %{name: "Shoes",display_name: ""}
-      cs = %{valid?: validity} = Property.update_changeset(property,params)
+      params = %{name: "Corsair Power Supply", display_name: ""}
+      cs = %{valid?: validity} = Property.update_changeset(property, params)
       refute validity
       assert %{display_name: ["can't be blank"]} = errors_on(cs)
     end
 
     test "name must be unique", %{property: property} do
-      params = %{name: "Shoes",display_name: "Nike"}
-      cset = Property.update_changeset(property,params)
-      assert {:ok,_} = Repo.insert(cset)
+      params = %{name: "Corsair Power Supply", display_name: "AC input"}
+      changeset = Property.update_changeset(property, params)
+      assert {:ok, _} = Repo.insert(changeset)
 
-      params = %{name: "Shoes",display_name: "Adidas"}
-      cset =  Property.update_changeset(property,params)
-      assert {:error,cs} = Repo.insert(cset)
+      params = %{name: "Corsair Power Supply", display_name: "Safety Approvals"}
+      changeset =  Property.update_changeset(property, params)
+      assert {:error, cs} = Repo.insert(changeset)
       assert %{name: ["has already been taken"]} = errors_on(cs)
     end
   end
