c9b55f5abc891dd9a08ae9ee6c98677f774c7c72
Suhas Dombale <suhasdombale123@gmail.com>
2019-01-16 00:09:32 +0530

    Added test cases for state model

--- apps/snitch_core/test/data/model/state_test.exs
+++ apps/snitch_core/test/data/model/state_test.exs
@@ -6,14 +6,34 @@ defmodule Snitch.Data.Model.StateTest do
 
   setup :states
 
-  describe "state formatting" do
+  describe "get/1 " do
+    test "succeeds", %{states: states} do
+      states = states |> List.first()
+      state = State.get(%{name: "California"})
+      assert state.name == states.name
+    end
+
+    test "Fails " do
+      state = State.get(%{name: "Japan"})
+      assert state == nil
+    end
+  end
+
+  describe "get_all/0 " do
+    test "succeeds", %{states: states} do
+      states_list = State.get_all()
+      assert length(states_list) == length(states)
+    end
+  end
+
+  describe "state formatting " do
     test "for all the states", %{states: states} do
       state = states |> List.first()
       states_list = State.formatted_list()
       assert states_list == [{"California", state.id}]
     end
 
-    test "for all the states belonging to a given country", %{states: states} do
+    test "for states belonging to a given country", %{states: states} do
       state = states |> List.first()
       country_states = State.formatted_state_list(state.country_id)
       assert country_states == [%{id: state.id, text: "California"}]
