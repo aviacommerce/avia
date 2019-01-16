22b0d8a4f7ceec4a646935186162ff3c4a2414a4
Jeshan <jeshankhn6@gmail.com>
2019-01-16 14:55:55 +0545

    [#163144092] Added test cases for Property model

---
+++ apps/snitch_core/test/data/model/property_test.exs
@@ -0,0 +1,70 @@
+defmodule Snitch.Data.Model.PropertyTest do
+  use ExUnit.Case
+  use Snitch.DataCase
+
+  import Snitch.Factory
+  alias Snitch.Data.Model.Property
+  alias Snitch.Data.Schema.Property, as: PropertySchema
+
+  @params %{
+    name: "Sapphire Radeon",
+    display_name: "processors"
+  }
+
+  describe "create/2" do
+    test "create sucessfully" do
+      assert {:ok, _} = Property.create(@params)
+    end
+
+    test "creation fails for duplicate name" do
+      property = insert(:property)
+      params = %{name: property.name, display_name: "AC Input"}
+      assert {:error, changeset} = Property.create(params)
+      assert %{name: ["has already been taken"]} = errors_on(changeset)
+    end
+  end
+
+  describe "update/2" do
+    test "update with " do
+      property = insert(:property)
+      %{id: expected_id} = property
+      updates = %{name: "Amazon TV"}
+      assert {:ok, %{id: received_id}} = Property.update(property, updates)
+      assert expected_id == received_id
+    end
+  end
+
+    test "get property" do
+      property = insert(:property)
+      assert property_returned = Property.get(property.id)
+      assert property_returned = property
+      assert {:ok, _} = Property.delete(property.id)
+      assert Property.get(property.id) == nil
+    end
+
+    test "get all properties" do
+      insert(:property)
+      assert Property.get_all() != []
+    end
+
+  describe "delete/1" do
+    test "delete a property" do
+      property = insert(:property)
+      assert {:ok, _} = Property.delete(property)
+      assert Repo.get(PropertySchema, property.id) == nil
+    end
+
+    test "deletion failed not found" do
+      assert {:error, :not_found} = Property.delete(-1)
+    end
+  end
+
+  describe "format/0" do
+    test "for all properties" do
+      properties = insert_list(1, :property)
+      property = properties |> List.first()
+      property_list = Property.get_formatted_list()
+      assert property_list == [{property.display_name, property.id}]
+    end
+  end
+end
