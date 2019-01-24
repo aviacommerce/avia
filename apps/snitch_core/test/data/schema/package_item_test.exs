ee1d926b6af4a1498e72ed61c528d40fc42cc62e
Jeshan <jeshankhn6@gmail.com>
2019-01-24 17:03:58 +0545

    [#163248592]    - Updated the test cases in schema/package_item_test.exs    - Wrote new test cases in schema/package_item_test.exs

--- apps/snitch_core/test/data/schema/package_item_test.exs
+++ apps/snitch_core/test/data/schema/package_item_test.exs
@@ -3,6 +3,7 @@ defmodule Snitch.Data.Schema.PackageItemTest do
   use Snitch.DataCase
 
   import Ecto.Changeset, only: [fetch_change: 2, apply_changes: 1]
+  import Snitch.Factory
 
   alias Snitch.Data.Schema.PackageItem
 
@@ -19,8 +20,12 @@ defmodule Snitch.Data.Schema.PackageItemTest do
     shipping_tax: nil
   }
 
+  setup :zones
+  setup :shipping_methods
+  setup :embedded_shipping_methods
+
   describe "create_changeset/2" do
-    test "with valid params, and backorder is computed correctly" do
+    test "suuceesfully with valid params, and backorder is computed correctly" do
       assert cs = %{valid?: true} = PackageItem.create_changeset(%PackageItem{}, @params)
       assert {:ok, true} == fetch_change(cs, :backordered?)
 
@@ -29,7 +34,7 @@ defmodule Snitch.Data.Schema.PackageItemTest do
       assert {:ok, false} == fetch_change(cs, :backordered?)
     end
 
-    test "with missing params" do
+    test "fails with missing params" do
       cs = PackageItem.create_changeset(%PackageItem{}, %{})
       refute cs.valid?
 
@@ -41,7 +46,34 @@ defmodule Snitch.Data.Schema.PackageItemTest do
              } == errors_on(cs)
     end
 
-    test "with invalid quantity, delta" do
+    test "fails for non-existent line_item_id", context do
+      %{package: package, product: product, line_item: line_item} = new_package(context)
+
+      params = %{@params | package_id: package.id, product_id: product.id, line_item_id: -1}
+      cs = PackageItem.create_changeset(%PackageItem{}, params)
+      {:error, changeset} = Repo.insert(cs)
+      assert %{line_item_id: ["does not exist"]} == errors_on(changeset)
+    end
+
+    test "fails for non-existent product_id", context do
+      %{package: package, product: product, line_item: line_item} = new_package(context)
+
+      params = %{@params | package_id: package.id, product_id: -1, line_item_id: line_item.id}
+      cs = PackageItem.create_changeset(%PackageItem{}, params)
+      {:error, changeset} = Repo.insert(cs)
+      assert %{product_id: ["does not exist"]} == errors_on(changeset)
+    end
+
+    test "fails for non-existent package_id", context do
+      %{package: package, product: product, line_item: line_item} = new_package(context)
+
+      params = %{@params | package_id: -1, product_id: product.id, line_item_id: line_item.id}
+      cs = PackageItem.create_changeset(%PackageItem{}, params)
+      {:error, changeset} = Repo.insert(cs)
+      assert %{package_id: ["does not exist"]} == errors_on(changeset)
+    end
+
+    test "fails with invalid quantity, delta" do
       cs = PackageItem.create_changeset(%PackageItem{}, %{@params | quantity: -2, delta: -1})
       refute cs.valid?
 
@@ -51,7 +83,7 @@ defmodule Snitch.Data.Schema.PackageItemTest do
              } = errors_on(cs)
     end
 
-    test "with invalid tax, shipping_tax" do
+    test "fails with invalid tax, shipping_tax" do
       bad_money = Money.new(-1, :USD)
 
       cs =
@@ -69,7 +101,7 @@ defmodule Snitch.Data.Schema.PackageItemTest do
   end
 
   describe "update_changeset/2" do
-    test "with valid params, and backorder is computed correctly" do
+    test "successful with valid params, and backorder is computed correctly" do
       assert cs = %{valid?: true} = PackageItem.create_changeset(%PackageItem{}, @params)
       package_item = apply_changes(cs)
       params = %{state: "destroyed", quantity: 0, delta: 3, backordered?: false}
@@ -91,5 +123,79 @@ defmodule Snitch.Data.Schema.PackageItemTest do
 
       assert {:ok, false} = fetch_change(cs, :backordered?)
     end
+
+    test "fails with invalid quantity, delta", context do
+      %{package_item: package_item} = make_package_item(context)
+      cs = PackageItem.create_changeset(%PackageItem{}, Map.from_struct(package_item))
+      refute cs.valid?
+
+      package_item = %{package_item | quantity: -3, delta: -2}
+      cs = PackageItem.update_changeset(%PackageItem{}, Map.from_struct(package_item))
+
+      assert %{
+               delta: ["must be greater than -1"],
+               quantity: ["must be greater than -1"]
+             } == errors_on(cs)
+    end
+
+    test "fails with invalid and valid tax, shipping tax", context do
+      bad_money = Money.new(-1, :USD)
+      %{package_item: package_item} = make_package_item(context)
+      cs = PackageItem.create_changeset(%PackageItem{}, Map.from_struct(package_item))
+      refute cs.valid?
+
+      package_item = %{package_item | tax: bad_money, shipping_tax: bad_money}
+      cs = PackageItem.update_changeset(%PackageItem{}, Map.from_struct(package_item))
+
+      assert %{
+               shipping_tax: ["must be equal or greater than 0"],
+               tax: ["must be equal or greater than 0"]
+             } = errors_on(cs)
+    end
+  end
+
+  defp new_package(context) do
+      %{embedded_shipping_methods: embedded_shipping_methods} = context
+
+      country = insert(:country)
+      state = insert(:state, country: country)
+      stock_location = insert(:stock_location, state: state)
+      stock_item = insert(:stock_item, count_on_hand: 10, stock_location: stock_location)
+      shipping_category = insert(:shipping_category)
+
+      product = stock_item.product
+
+      order = insert(:order, state: "delivery")
+
+      line_item = insert(:line_item, order: order, product: product, quantity: 3)
+
+      package =
+        insert(:package,
+          shipping_methods: embedded_shipping_methods,
+          order: order,
+          items: [],
+          origin: stock_item.stock_location,
+          shipping_category: shipping_category
+        )
+
+        %{package: package, line_item: line_item, product: product}
+      end
+
+      defp make_package_item(context) do
+        %{package: package, line_item: line_item, product: product} = new_package(context)
+
+          package_item =
+            insert(:package_item,
+            quantity: 3,
+            product: product,
+            line_item: line_item,
+            package: package
+          )
+
+        package = package_item.package
+          product = package_item.product
+          line_item = package_item.line_item
+
+       %{package_item: package_item}
   end
 end
