394cfd659609c04f5a9fb4d69df351841e4a6e5b
Jeshan <jeshankhn6@gmail.com>
2019-01-30 12:40:07 +0545

    [#163215222]
    Prior to this change,
      - params were patternmatched inside the test
      - In update_changset/2 params were given to empty struct
    
    This change
      - params are  now patternmatched in test statement
      - In update_changeset/2 params are given to product_property
        struct which is already presnt in database

--- apps/snitch_core/test/data/schema/product_property_test.exs
+++ apps/snitch_core/test/data/schema/product_property_test.exs
@@ -15,12 +15,11 @@ defmodule Snitch.Data.Schema.ProductPropertyTest do
       value: product_property.value
     }
 
-    [params: params]
+    [params: params, product_property: product_property]
   end
 
   describe "create_changeset/2" do
-    test "successfully with valid params", context do
-      %{params: params} = context
+    test "successfully with valid params", %{params: params} do
       cs = ProductProperty.create_changeset(%ProductProperty{}, params)
       assert cs.valid?
     end
@@ -28,14 +27,15 @@ defmodule Snitch.Data.Schema.ProductPropertyTest do
     test "fails with invalid params" do
       cs = ProductProperty.create_changeset(%ProductProperty{}, %{})
       refute cs.valid?
-      assert %{ product_id: ["can't be blank"],
-                property_id: ["can't be blank"],
-                value: ["can't be blank"],
-               } == errors_on(cs)
+
+      assert %{
+               product_id: ["can't be blank"],
+               property_id: ["can't be blank"],
+               value: ["can't be blank"]
+             } == errors_on(cs)
     end
 
-    test "fails for duplicate product_id", context do
-      %{params: params} = context
+    test "fails for duplicate product_id", %{params: params} do
       cs = ProductProperty.create_changeset(%ProductProperty{}, params)
       {:error, changeset} = Repo.insert(cs)
       assert %{property_id: ["has already been taken"]} == errors_on(changeset)
@@ -46,60 +46,39 @@ defmodule Snitch.Data.Schema.ProductPropertyTest do
         product_id: -1,
         property_id: 1,
         value: "val"
-     }
+      }
 
       cs = ProductProperty.create_changeset(%ProductProperty{}, params)
       {:error, changeset} = Repo.insert(cs)
-      assert %{product_id: ["does not exist"]
-            } == errors_on(changeset)
+      assert %{product_id: ["does not exist"]} == errors_on(changeset)
     end
 
-    test "fails for non-existent property_id", context do
-      %{params: params} = context
+    test "fails for non-existent property_id", %{params: params} do
       params = %{
         product_id: params.product_id,
         property_id: -1,
         value: "val"
-     }
+      }
 
       cs = ProductProperty.create_changeset(%ProductProperty{}, params)
       {:error, changeset} = Repo.insert(cs)
-      assert %{property_id: ["does not exist"]
-            } == errors_on(changeset)
+      assert %{property_id: ["does not exist"]} == errors_on(changeset)
     end
   end
 
   describe "update_changeset/2" do
-    setup do
-      product_property = insert(:product_property)
-
-      params = %{
-        product_id: product_property.product_id,
-        property_id: product_property.property_id,
-        value: product_property.value
-      }
-        [
-          changset:
-          %ProductProperty{}
-          |> ProductProperty.create_changeset(params)
-          |> apply_changes(),
-
-          params: params
-        ]
-    end
-
-    test "successfully with valid params", %{changset: product_property, params: params} do
-      params = %{params | value: "val"}
-      updated_changeset = ProductProperty.update_changeset(%ProductProperty{}, params)
+    test "successfully with valid params", %{product_property: product_property} do
+      params = %{value: "val"}
+      updated_changeset = ProductProperty.update_changeset(product_property, params)
       assert updated_changeset.valid?
     end
 
-    test "fails for invalid params", %{changset: product_property, params: params} do
-      params = %{params | property_id: nil, value: ""}
-      updated_changeset = ProductProperty.update_changeset(%ProductProperty{}, params)
-      assert %{ property_id: ["can't be blank"],
-                value: ["can't be blank"],
-               } == errors_on(updated_changeset)
+    test "fails for invalid params", %{product_property: product_property} do
+      params = %{property_id: nil, value: ""}
+      updated_changeset = ProductProperty.update_changeset(product_property, params)
+
+      assert %{property_id: ["can't be blank"], value: ["can't be blank"]} ==
+               errors_on(updated_changeset)
     end
   end
 end
