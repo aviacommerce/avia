01c4d6971f476ea66c001db7334dbf918c63be56
Jeshan <jeshankhn6@gmail.com>
2019-01-31 18:16:13 +0545

    [#163586461]
    Prior to this change,
      In Shipping_category (Model)
       - Test for get_with_rules/1 was not present.
    
      In Shipping_method (Model)
       - Tests for get/1, get_all/0, delete/1 and for_package_query/2
         were not present.
    
      In ShippingRuleIdentifier (Schema)
       - Test file was not present.
    
      Shipping identifier schema file name was wrong.
    This change
      In Shipping_category (Model)
       - Added test for get_with_rules/1.
    
      In Shipping_method (Model)
       - Added tests for get/1, get_all/0, delete/1 and for_package_query/2 .
    
      In ShippingRuleIdentifier (Schema)
       - Created a new test file.
    
      Renamed the file schema/shipping_rules/shipping_rule_idenitifier.ex
      to schema/shipping_rules/shipping_rule_identifier.ex

---
+++ apps/snitch_core/test/data/schema/shipping_rules/shipping_rule_identifier_test.exs
@@ -0,0 +1,58 @@
+defmodule Snitch.Data.Schema.ShippingRuleIdentifierTest do
+  use ExUnit.Case, async: true
+  use Snitch.DataCase
+
+  import Snitch.Factory
+
+  alias Snitch.Data.Schema.ShippingRuleIdentifier, as: SRIdentifier
+
+  setup do
+    si = insert(:shipping_identifier)
+    params = Map.from_struct(si)
+
+    [params: params]
+  end
+
+  describe "changeset/2" do
+    test "successful for valid params", %{params: params} do
+      cs = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert cs.valid?
+    end
+
+    test "fails for invalid params", %{params: params} do
+      params = %{params | code: nil, description: ""}
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert %{code: ["can't be blank"], description: ["can't be blank"]} == errors_on(changeset)
+    end
+
+    test "fails for duplicate code value", %{params: params} do
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      {:error, changeset} = Repo.insert(changeset)
+      assert %{code: ["has already been taken"]} == errors_on(changeset)
+    end
+
+    test "returns a valid changeset for valid code value", %{params: params} do
+      params = %{params | code: :fsoa, description: "free shipping above specified amount"}
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert changeset.valid?
+
+      params = %{params | code: :fsrp, description: "fixed shipping rate per product"}
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert changeset.valid?
+
+      params = %{params | code: :ofr, description: "fixed shipping rate for order"}
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert changeset.valid?
+
+      params = %{params | code: :fso, description: "free shipping for order"}
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert changeset.valid?
+    end
+
+    test "fails for invalid code value", %{params: params} do
+      params = %{params | code: :ofrr}
+      changeset = SRIdentifier.changeset(%SRIdentifier{}, params)
+      assert %{code: ["is invalid"]} == errors_on(changeset)
+    end
+  end
+end
