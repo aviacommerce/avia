e71a318e38d4be4d99413c976a4a859430a5bf22
Jeshan7 <32778335+Jeshan7@users.noreply.github.com>
2019-01-31 18:26:49 +0545

    [test] CountryZoneMember and StateZoneMember (model/schema) (#454)
    - adds test for CountryZone and StateZone

--- apps/snitch_core/test/data/schema/zone/country_zone_member_test.exs
+++ apps/snitch_core/test/data/schema/zone/country_zone_member_test.exs
@@ -8,6 +8,21 @@ defmodule Snitch.Data.Schema.CountryZoneMemberTest do
 
   setup :countries
 
+  describe "create_changeset/2" do
+    test "successfully for valid params", %{countries: [country]} do
+      zone = insert(:zone, zone_type: "C")
+      params = %{zone_id: zone.id, country_id: country.id}
+      cs = CountryZoneMember.create_changeset(%CountryZoneMember{}, params)
+      assert cs.valid?
+    end
+
+    test "fails for invalid params" do
+      params = %{zone_id: nil, country_id: nil}
+      cs = CountryZoneMember.create_changeset(%CountryZoneMember{}, params)
+      assert %{zone_id: ["can't be blank"], country_id: ["can't be blank"]} == errors_on(cs)
+    end
+  end
+
   describe "CountryZoneMember records" do
     test "refer only country type zones", %{countries: [country]} do
       country_zone = insert(:zone, zone_type: "C")
@@ -42,6 +57,41 @@ defmodule Snitch.Data.Schema.CountryZoneMemberTest do
 
       assert {:ok, _} = Repo.insert(czm_changset)
       assert {:error, cs} = Repo.insert(czm_changset)
+      assert %{country_id: ["has already been taken"]} = errors_on(cs)
+    end
+  end
+
+  describe "update_changeset/2" do
+    setup %{countries: [country]} do
+      zone = insert(:zone, zone_type: "C")
+      params = %{zone_id: zone.id, country_id: country.id}
+
+      [
+        cs:
+          %CountryZoneMember{}
+          |> CountryZoneMember.create_changeset(params)
+          |> apply_changes()
+      ]
+    end
+
+    test "returns a valid changeset", %{cs: cs} do
+      params = %{country_id: 200}
+      changeset = CountryZoneMember.update_changeset(cs, params)
+      assert changeset.valid?
+    end
+
+    test "fails for invalid params", %{cs: cs} do
+      params = %{country_id: -1}
+      changeset = CountryZoneMember.update_changeset(cs, params)
+      {:error, changeset} = Repo.insert(changeset)
+      assert %{country_id: ["does not exist"]} == errors_on(changeset)
+    end
+
+    test "fails for duplicate country_id", %{cs: cs} do
+      Repo.insert(cs)
+      params = %{country_id: cs.country_id}
+      changeset = CountryZoneMember.update_changeset(cs, params)
+      {:error, cs} = Repo.insert(changeset)
       assert %{country_id: ["has already been taken"]} = errors_on(cs)
     end
   end
