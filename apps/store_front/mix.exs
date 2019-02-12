1b21466079b299268679b08ff27165704960f5a2
Pankaj Rawat <pankajrawat19sept@gmail.com>
2019-02-12 14:42:01 +0530

    added snitch_core dep in store front

--- apps/store_front/mix.exs
+++ apps/store_front/mix.exs
@@ -42,7 +42,8 @@ defmodule StoreFront.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:snitch_core, "~> 0.0.1", in_umbrella: true}
     ]
   end
 end
