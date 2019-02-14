5f9b9e01b44b191e6007948d4dd31fa410a99777
Jeshan <jeshankhn6@gmail.com>
2019-02-14 22:52:43 +0545

    [#163248658] This change   - Added rating_option tests(schema)   - Refactored the rating.ex (factory)

--- apps/snitch_core/test/data/schema/rating/rating_option_test.exs
+++ apps/snitch_core/test/data/schema/rating/rating_option_test.exs
@@ -4,37 +4,44 @@ defmodule Snitch.Data.Schema.RatingOptionTest do
   import Snitch.Factory
   alias Snitch.Data.Schema.RatingOption
 
-  @valid_attrs %{
-    code: "1",
-    value: 1,
-    position: 1,
-    rating_id: 147
-  }
-  describe "create_changeset/2 " do
-    test "succeeds" do
-      %{valid?: validity} = RatingOption.create_changeset(%RatingOption{}, @valid_attrs)
-      assert validity
+  setup :rating_options
+
+  @tag rating_option_count: 1
+  describe "create_changeset/2" do
+    test "returns a valid changeset", %{rating_options: [rating_option]} do
+      rating_option = Map.from_struct(rating_option)
+      changeset = RatingOption.create_changeset(%RatingOption{}, rating_option)
+      assert changeset.valid?
     end
 
-    test "fails? with all empty" do
-      c = %{valid?: validity} = RatingOption.create_changeset(%RatingOption{}, %{})
-      refute validity
+    test "fails for invalid params" do
+      changeset = RatingOption.create_changeset(%RatingOption{}, %{})
+      refute changeset.valid?
+
+      assert %{
+               code: ["can't be blank"],
+               value: ["can't be blank"],
+               position: ["can't be blank"],
+               rating_id: ["can't be blank"]
+             } == errors_on(changeset)
     end
   end
 
   describe "update_changeset/2 " do
-    test "succeeds" do
-      rating_opt = insert(:rating_option)
-      c = RatingOption.update_changeset(rating_opt, %{position: 2})
-      assert {:ok, new} = Repo.update(c)
-      assert new.position != rating_opt.position
+    @tag rating_option_count: 1
+    test "returns a valid changeset", %{rating_options: [rating_option]} do
+      params = %{position: 43}
+      changeset = RatingOption.update_changeset(rating_option, params)
+      {:ok, new} = Repo.update(changeset)
+      assert new.position != rating_option.position
     end
 
-    test "fails? with required param nil" do
-      rating_opt = insert(:rating_option)
-      c = RatingOption.update_changeset(rating_opt, %{position: nil, code: nil})
-      refute c.valid?
-      assert %{position: ["can't be blank"], code: ["can't be blank"]} == errors_on(c)
+    @tag rating_option_count: 1
+    test "fails for invalid params", %{rating_options: [rating_option]} do
+      params = %{position: nil, code: nil}
+      changeset = RatingOption.update_changeset(rating_option, params)
+      refute changeset.valid?
+      assert %{position: ["can't be blank"], code: ["can't be blank"]} == errors_on(changeset)
     end
   end
 end
