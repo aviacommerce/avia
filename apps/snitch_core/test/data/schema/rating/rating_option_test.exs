e130447d0ece21f770e2350fc6387c7e4770f983
Jeshan <jeshankhn6@gmail.com>
2019-02-21 23:01:12 +0545

    [#163248658]  Prior to this change,  - foreign_key_constraint for rating_id was not present.  - Directly rating_options from factory was passed as context in create/2
     This change
     - foreign_key_constraint for rating_id is now present.
     - Wrote test case for foreign_key_constraint,
     - Now params are passes through context in create/2.

--- apps/snitch_core/test/data/schema/rating/rating_option_test.exs
+++ apps/snitch_core/test/data/schema/rating/rating_option_test.exs
@@ -6,14 +6,25 @@ defmodule Snitch.Data.Schema.RatingOptionTest do
 
   setup :rating_options
 
-  @tag rating_option_count: 1
   describe "create_changeset/2" do
-    test "returns a valid changeset", %{rating_options: [rating_option]} do
-      rating_option = Map.from_struct(rating_option)
-      changeset = RatingOption.create_changeset(%RatingOption{}, rating_option)
+    setup %{rating_options: [rating_option]} do
+      params = %{
+        code: rating_option.code,
+        value: rating_option.value,
+        position: rating_option.position,
+        rating_id: rating_option.rating_id
+      }
+
+      [params: params]
+    end
+
+    @tag rating_option_count: 1
+    test "returns a valid changeset", %{params: params} do
+      changeset = RatingOption.create_changeset(%RatingOption{}, params)
       assert changeset.valid?
     end
 
+    @tag rating_option_count: 1
     test "fails for invalid params" do
       changeset = RatingOption.create_changeset(%RatingOption{}, %{})
       refute changeset.valid?
@@ -24,6 +35,14 @@ defmodule Snitch.Data.Schema.RatingOptionTest do
                position: ["can't be blank"],
                rating_id: ["can't be blank"]
              } == errors_on(changeset)
+    end
+
+    @tag rating_option_count: 1
+    test "fails for non-existent rating_id", %{params: params} do
+      params = %{params | rating_id: -1}
+      cs = RatingOption.create_changeset(%RatingOption{}, params)
+      {:error, changeset} = Repo.insert(cs)
+      assert %{rating_id: ["does not exist"]} == errors_on(changeset)
     end
   end
 
