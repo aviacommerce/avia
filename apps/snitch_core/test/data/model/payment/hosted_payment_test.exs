54d264fc179b5b5f17d174854bdca063e1d935e9
Jeshan7 <32778335+Jeshan7@users.noreply.github.com>
2019-03-04 19:55:48 +0530

    [tests] test for payment and hosted payment (#472)
    Why?
    Test cases for get/1, get_all/0 and from_payment/1 were not present.
    In hosted_payment.ex (model)
    Test cases for get_all/0 and delete/1 were not present.
    
    This change addresses the need by:
    Test cases for get/1, get_all/0 and from_payment/1 are now present.
    In hosted_payment.ex (model)
    Test cases for get_all/0 and delete/1 are now present.
    Refactored some lines of payment_test.exs and hosted_payment_test.exs
    
    [delivers #163829106]

--- apps/snitch_core/test/data/model/payment/hosted_payment_test.exs
+++ apps/snitch_core/test/data/model/payment/hosted_payment_test.exs
@@ -17,39 +17,134 @@ defmodule Snitch.Data.Model.HostedPaymentTest do
   setup :payment_methods
   setup :hosted_payment
 
-  test "create/5 hosted payment and related payment", context do
-    %{hosted_method: payment_method} = context
-    %{order: order} = context
-    payment_params = payment_params()
-    hosted_payment_params = hosted_payment_params()
-    slug = "hosted-payment" <> to_string(:rand.uniform(1000))
-    payment_method_id = payment_method.id
-    order_id = order.id
+  describe "create/5" do
+    test "creates hosted payment and related payment successfully", context do
+      %{hosted_method: payment_method} = context
+      %{order: order} = context
+      payment_params = payment_params()
+      hosted_payment_params = hosted_payment_params()
+      slug = "hosted-payment" <> to_string(:rand.uniform(1000))
+      payment_method_id = payment_method.id
+      order_id = order.id
 
-    assert {:ok, %{payment: payment, hosted_payment: hosted_payment}} =
-             HostedPayment.create(
-               slug,
-               order_id,
-               payment_params,
-               hosted_payment_params,
-               payment_method_id
-             )
+      assert {:ok, %{payment: payment, hosted_payment: hosted_payment}} =
+               HostedPayment.create(
+                 slug,
+                 order_id,
+                 payment_params,
+                 hosted_payment_params,
+                 payment_method_id
+               )
 
-    assert payment.state == "pending"
-    assert payment.amount == Money.zero(:USD)
-    assert hosted_payment.payment_id == payment.id
+      assert payment.state == "pending"
+      assert payment.amount == Money.zero(:USD)
+      assert hosted_payment.payment_id == payment.id
+    end
+
+    test "fails for non_existent order_id", context do
+      %{hosted_method: payment_method} = context
+      %{order: order} = context
+      payment_params = payment_params()
+      hosted_payment_params = hosted_payment_params()
+      slug = "hosted-payment" <> to_string(:rand.uniform(1000))
+      payment_method_id = payment_method.id
+      order_id = -1
+
+      {:error, changeset} =
+        HostedPayment.create(
+          slug,
+          order_id,
+          payment_params,
+          hosted_payment_params,
+          payment_method_id
+        )
+
+      assert %{order_id: ["does not exist"]} == errors_on(changeset)
+    end
+
+    test "fails for duplicate slug", context do
+      %{hosted_method: payment_method} = context
+      %{order: order} = context
+      payment_params = payment_params()
+      hosted_payment_params = hosted_payment_params()
+      slug = "hosted-payment" <> to_string(:rand.uniform(1000))
+      payment_method_id = payment_method.id
+      order_id = order.id
+
+      {:ok, _} =
+        HostedPayment.create(
+          slug,
+          order_id,
+          payment_params,
+          hosted_payment_params,
+          payment_method_id
+        )
+
+      {:error, changeset} =
+        HostedPayment.create(
+          slug,
+          order_id,
+          payment_params,
+          hosted_payment_params,
+          payment_method_id
+        )
+
+      assert %{slug: ["has already been taken"]} == errors_on(changeset)
+    end
   end
 
-  test "update/3", %{hosted_payment: hosted_payment, payment: payment} do
-    hosted_params = %{transaction_id: "1234", raw_response: %{status: "success"}}
-    payment_params = %{amount: Money.new(-1, :USD), state: "complete"}
+  describe "update/3" do
+    test "updates hosted_payment and related_payment successfully", %{
+      hosted_payment: hosted_payment,
+      payment: payment
+    } do
+      hosted_params = %{transaction_id: "1234", raw_response: %{status: "success"}}
+      payment_params = %{amount: Money.new(-1, :USD), state: "complete"}
 
-    assert {:ok, %{payment: updated_payment, hosted_payment: updated_hosted_payment}} =
-             HostedPayment.update(hosted_payment, hosted_params, payment_params)
+      assert {:ok, %{payment: updated_payment, hosted_payment: updated_hosted_payment}} =
+               HostedPayment.update(hosted_payment, hosted_params, payment_params)
 
-    assert updated_payment.state == payment_params.state
-    assert Money.reduce(updated_payment.amount) == payment.amount
-    assert updated_hosted_payment.transaction_id == hosted_params.transaction_id
+      assert updated_payment.state == payment_params.state
+      assert Money.reduce(updated_payment.amount) == payment.amount
+      assert updated_hosted_payment.transaction_id == hosted_params.transaction_id
+    end
+
+    test "fails for invalid transaction_id and payment_source", %{
+      hosted_payment: hosted_payment,
+      payment: payment
+    } do
+      hosted_params = %{transaction_id: 10, payment_source: 20}
+      {:error, changeset} = HostedPayment.update(hosted_payment, hosted_params, payment_params)
+      assert %{transaction_id: ["is invalid"], payment_source: ["is invalid"]}
+    end
+  end
+
+  describe "get/1" do
+    test "returns a hosted payment with valid id", %{hosted_payment: hosted_payment} do
+      {:ok, returned_hosted_payment} = HostedPayment.get(hosted_payment.id)
+      assert hosted_payment.id == returned_hosted_payment.id
+    end
+
+    test "fails for invalid id" do
+      assert {:error, :hosted_payment_not_found} = HostedPayment.get(-1)
+    end
+
+    test "returns a hosted payment with  query params", %{hosted_payment: hosted_payment} do
+      map = %{payment_id: hosted_payment.payment_id}
+      {:ok, returned_hosted_payment} = HostedPayment.get(map)
+      assert returned_hosted_payment.id == hosted_payment.id
+    end
+  end
+
+  test "get_all/0", %{hosted_payment: hosted_payment, payment: payment} do
+    returned_hosted_payments = HostedPayment.get_all()
+    assert returned_hosted_payments != []
+  end
+
+  test "from_payment/1 returns a hosted payment", %{hosted_payment: hosted_payment} do
+    id = hosted_payment.payment_id
+    returned_hosted_payment = HostedPayment.from_payment(id)
+    assert returned_hosted_payment.id == hosted_payment.id
   end
 
   defp payment_params() do
--- apps/snitch_core/test/data/schema/payment/payment_test.exs
+++ apps/snitch_core/test/data/schema/payment/payment_test.exs
@@ -12,27 +12,21 @@ defmodule Snitch.Data.Schema.PaymentTest do
 
   setup :payment_methods
 
-  test "Payments invalidate bad type", context do
-    %{check_method: method, order: order} = context
-
+  test "Payments invalidate bad type", %{check_method: method, order: order} do
     check_payment =
       :payment_chk
       |> build(payment_method_id: method.id, order_id: order.id)
       |> Payment.create_changeset(%{payment_type: "abc"})
 
-    assert %Ecto.Changeset{errors: errors} = check_payment
-    assert errors == [payment_type: {"is invalid", [validation: :inclusion]}]
+    assert %{payment_type: ["is invalid"]} == errors_on(check_payment)
   end
 
-  test "Payments cannot have negative amount", context do
-    %{check_method: method, order: order} = context
-
+  test "Payments cannot have negative amount", %{check_method: method, order: order} do
     check_payment =
       :payment_chk
       |> build(payment_method_id: method.id, order_id: order.id)
       |> Payment.create_changeset(%{amount: Money.new("-0.0001", :USD)})
 
-    assert %Ecto.Changeset{errors: errors} = check_payment
-    assert errors == [amount: {"must be equal or greater than 0", [validation: :number]}]
+    assert %{amount: ["must be equal or greater than 0"]} == errors_on(check_payment)
   end
 end
