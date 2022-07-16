386a1ac414b7aeed12bc218a2f8e0d6da3da724e
pkrawat1 <pankajrawat19sept@gmail.com>
2022-07-16 19:10:39 +0530

    upgrade elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Snitch.Mixfile do
   def project do
     [
       apps_path: "apps",
-      elixir: "~> 1.7.2",
+      elixir: "~> 1.13.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
